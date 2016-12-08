ac7dbd2642d799e8163763eb9e16752c52a0715c
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-12-07 16:26:09 -0800

    Makes WRF example depend on docker only
    Runs ansible in a container.

--- templates/experiments/wrf-singlenode-numeric-reproducibility/run.sh
+++ templates/experiments/wrf-singlenode-numeric-reproducibility/run.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-sh -c "cd ansible/ && \
-       ansible-playbook -e @../vars.yml -e results_path=../results playbook.yml"
 
-# The following executes the experiment using docker (won't work on OSXs)
-#
-#docker run --rm -ti \
-#  -v `pwd`/ansible:/experiment \
-#  -v `pwd`/vars.yml:/experiment/vars.yml \
-#  -v `pwd`/results:/results \
-#  -v "$SSH_AUTH_SOCK:/tmp/ssh_auth_sock" \
-#  -e "SSH_AUTH_SOCK=/tmp/ssh_auth_sock" \
-#  --workdir=/experiment \
-#  --net=host \
-#  --entrypoint=/bin/bash \
-#  ivotron/ansible:2.2.0.0 -c \
-#    "ssh-add -l && \
-#     ansible-playbook -e @vars.yml -e results_path=/results playbook.yml"
+docker run --rm -ti \
+  -v `pwd`/ansible:/experiment \
+  -v `pwd`/vars.yml:/experiment/vars.yml \
+  -v `pwd`/results:/results \
+  -v /usr/bin/docker:/usr/bin/docker \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $HOME/.ssh/id_rsa:/root/.ssh/id_rsa \
+  -v $HOME/.ssh/id_dsa:/root/.ssh/id_dsa \
+  --workdir=/experiment \
+  --net=host \
+  --entrypoint=/bin/bash \
+  ivotron/ansible:2.2.0.0 -c \
+    "ansible-playbook -e @vars.yml -e results_path=/results playbook.yml"
