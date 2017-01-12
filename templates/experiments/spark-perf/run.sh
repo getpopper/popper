ed0e8dfb8bf3899ad58d49eb9549506c669aa1ff
Ivo Jimenez <ivo.jimenez@gmail.com>
2017-01-11 16:20:28 -0800

    Updates spark-perf ML experiment to make use of docker-bench

--- templates/experiments/spark-perf/run.sh
+++ templates/experiments/spark-perf/run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e -x
 
+# delete previous results
+sudo rm -f results/runtime_*
+sudo rm -fr results/machine/*
+
 docker run --rm -ti \
-  -v `pwd`:/experiment \
-  -v "$SSH_AUTH_SOCK:/tmp/ssh_auth_sock" \
-  -e "SSH_AUTH_SOCK=/tmp/ssh_auth_sock" \
+  -v `pwd`/ansible:/experiment \
+  -v `pwd`/../../vendor/baseliner:/experiment/roles/baseliner \
+  -v `pwd`/results:/results \
+  -v $HOME/.ssh/issdm_rsa:/root/.ssh/id_rsa \
   --workdir=/experiment \
   --net=host \
-  --entrypoint=/experiment/.entrypoint.sh \
-  williamyeh/ansible:debian8
-
+  --entrypoint=/bin/bash \
+  ivotron/ansible:2.2.0.0 -c \
+    "ansible-playbook -e results_path=/results/output -e @vars.yml playbook.yml"
