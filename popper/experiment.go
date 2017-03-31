09029f88cc33f371fc66bdbd046fcf033f9adb52
Ivo Jimenez <ivo.jimenez@gmail.com>
2017-03-31 15:57:06 -0700

    Adds popper check command to CLI
    This is the other part of this issue, on the CLI side.
    
    fixes #67

--- popper/experiment.go
+++ popper/experiment.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"path"
+	"strings"
 
 	"github.com/codeskyblue/go-sh"
 	"github.com/spf13/cobra"
@@ -52,6 +54,23 @@ func addTemplate(template_type string, template_name string, folder string) {
 	if _, err := sh.Command("cp", "-r", template, folder).CombinedOutput(); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func getRepoInfo() (user, repo string, err error) {
+	remoteURL, err := sh.Command(
+		"git", "config", "--get", "remote.origin.url").Output()
+	if err != nil {
+		return
+	}
+	urlAndUser, repo := path.Split(string(remoteURL))
+
+	// get the user or org name
+	user = path.Base(strings.Replace(urlAndUser, ":", "/", -1))
+
+	// trim and remove .git extension, if present
+	repo = strings.TrimSuffix(strings.TrimSpace(repo), ".git")
+
+	return
 }
 
 func initExperiment(name string) {
