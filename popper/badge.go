a591f9e8929e9f2bc4ca79aac10ea0108f3d2eda
Pete Wilcox <petercwilcox@gmail.com>
2017-10-27 12:23:49 -0700

    Badge service prototyped, needs testing

--- popper/badge.go
+++ popper/badge.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -32,59 +30,63 @@ var serviceCmd = &cobra.Command{
 
 		router := mux.NewRouter().StrictSlash(true)
 
+		// Update experiment status
 		router.
-			HandleFunc("/{orgId}/{repoId}/{expId}", handleExperiment).
-			Methods("GET", "POST")
+			HandleFunc("/{orgId}/{repoId}/{expId}/{sha}/{status}", handleExperiment).
+			Methods("POST")
+
+		// Get most recent badge
 		router.
-			HandleFunc("/{orgId}/{repoId}/{expId}/status.svg", handleBadge).
+			HandleFunc("/{orgId}/{repoId}/{expId}/status.svg", getLatestBadge).
+			Methods("Get")
+
+		// Get badge for specific SHA
+		router.
+			HandleFunc("/{orgId}/{repoId}/{expId}/{sha}/status.svg", getSpecificBadge).
 			Methods("GET")
 
 		log.Fatal(http.ListenAndServe(":9090", router))
 	},
 }
 
-type ExperimentStatus struct {
-	Name   string `json:"name"`
-	Status string `json:"status"`
-}
-
-func getExperimentStatus(w http.ResponseWriter, orgId, repoId, expId string) (exp *ExperimentStatus) {
-	exp = nil
-
+func getExperimentStatus(w http.ResponseWriter, orgId, repoId, expId, sha string) (expStatus string) {
+	// Open database, creates if necessary
 	db, err := bolt.Open("status.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Close database when function ends
 	defer db.Close()
 
+	// Identify buckets based on experiment ID
 	err = db.View(func(tx *bolt.Tx) error {
 		orgBucket := tx.Bucket([]byte(orgId))
 		if orgBucket == nil {
-			exp.Status = "invalid"
+			expStatus = "invalid"
 			return nil
 		}
 
 		repoBucket := orgBucket.Bucket([]byte(repoId))
 		if repoBucket == nil {
-			exp.Status = "invalid"
+			expStatus = "invalid"
 			return nil
 		}
 
 		expBucket := repoBucket.Bucket([]byte(expId))
 		if expBucket == nil {
-			exp.Status = "invalid"
+			expStatus = "invalid"
 			return nil
 		}
-
-		exp = expBucket.Get([]byte("status"))
-
+		// Each experiment is a bucket with SHA's stored as keys, status as value
+		status := expBucket.Get([]byte(sha))
+		expStatus = string(status)
 		return nil
 	})
 
-	return
+	return expStatus
 }
 
-//var repos = map[string]map[string]*ExperimentStatus{}
 var badges = map[string][]byte{
 	"invalid": []byte(`<svg xmlns="http://www.w3.org/2000/svg" width="108" height="20"><linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><mask id="a"><rect width="108" height="20" rx="3" fill="#fff"/></mask><g mask="url(#a)"><path fill="#555" d="M0 0h47v20H0z"/><path fill="#9f9f9f" d="M47 0h61v20H47z"/><path fill="url(#b)" d="M0 0h108v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="23.5" y="15" fill="#010101" fill-opacity=".3">Popper</text><text x="23.5" y="14">Popper</text><text x="76.5" y="15" fill="#010101" fill-opacity=".3">unknown</text><text x="76.5" y="14">unknown</text></g></svg>`),
 	"fail":    []byte(`<svg xmlns="http://www.w3.org/2000/svg" width="82" height="20"><linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><mask id="a"><rect width="82" height="20" rx="3" fill="#fff"/></mask><g mask="url(#a)"><path fill="#555" d="M0 0h47v20H0z"/><path fill="#e05d44" d="M47 0h35v20H47z"/><path fill="url(#b)" d="M0 0h82v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="23.5" y="15" fill="#010101" fill-opacity=".3">Popper</text><text x="23.5" y="14">Popper</text><text x="63.5" y="15" fill="#010101" fill-opacity=".3">FAIL</text><text x="63.5" y="14">FAIL</text></g></svg>`),
@@ -93,111 +95,95 @@ var badges = map[string][]byte{
 }
 
 func handleExperiment(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	orgId := vars["orgId"]
 	repoId := vars["repoId"]
 	expId := vars["expId"]
+	sha := vars["sha"]
+	status := vars["status"]
 
-	switch r.Method {
-	case "GET":
-		exp := getExperimentStatus(w, orgId, repoId, expId)
-		if exp.Status == "invalid" {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintln(w, string("Repo or user not found"))
-		}
-		outgoingJSON, error := json.Marshal(exp)
-
-		if error != nil {
-			log.Println(error.Error())
-			http.Error(w, error.Error(), http.StatusInternalServerError)
-			return
-		}
-		fmt.Fprint(w, string(outgoingJSON))
-
-	case "POST":
-		experiment := new(ExperimentStatus)
-		decoder := json.NewDecoder(r.Body)
-		error := decoder.Decode(&experiment)
-		if error != nil {
-			log.Println(error.Error())
-			http.Error(w, error.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		db, err := bolt.Open("status.db", 0600, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer db.Close()
-		db.Update(func(tx *bolt.Tx) error {
-			orgBucket, err := tx.CreateBucketIfNotExists([]byte(orgId))
-			if err != nil {
-				log.Println(error.Error())
-				http.Error(w, error.Error(), http.StatusInternalServerError)
-				return nil
-			}
-			repoBucket, err := orgBucket.CreateBucketIfNotExists([]byte(repoId))
-			if err != nil {
-				log.Println(error.Error())
-				http.Error(w, error.Error(), http.StatusInternalServerError)
-				return nil
-			}
-			expBucket, err := repoBucket.CreateBucketIfNotExists([]byte(expId))
-			if err != nil {
-				log.Println(error.Error())
-				http.Error(w, error.Error(), http.StatusInternalServerError)
-				return nil
-			}
-			experiment.Name = expId
-			expBucket.Put([]byte("status"), []byte(experiment))
-
-			return nil
-		})
-
-		//repo[expId] = experiment
-
-		outgoingJSON, err := json.Marshal(experiment)
-		if err != nil {
-			log.Println(error.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusCreated)
-		fmt.Fprint(w, string(outgoingJSON))
+	// Open db
+	db, err := bolt.Open("status.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	// Close when handler is finished
+	defer db.Close()
+
+	// Create status entry
+	// Creates buckets as necessary (for new entries)
+	db.Update(func(tx *bolt.Tx) error {
+		orgBucket, err := tx.CreateBucketIfNotExists([]byte(orgId))
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return nil
+		}
+		repoBucket, err := orgBucket.CreateBucketIfNotExists([]byte(repoId))
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return nil
+		}
+		expBucket, err := repoBucket.CreateBucketIfNotExists([]byte(expId))
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return nil
+		}
+		expBucket.Put([]byte(sha), []byte(status))
+
+		// Update the current key with the latest status to make it easy to grab
+		expBucket.Put([]byte("current"), []byte(status))
+		return nil
+	})
+
 }
 
-func handleBadge(w http.ResponseWriter, r *http.Request) {
+// Two wrappers for getBadge to return either a specific badge or just the latest one
+func getLatestBadge(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	orgId := vars["orgId"]
 	repoId := vars["repoId"]
 	expId := vars["expId"]
+	sha := "current"
 
-	switch r.Method {
-	case "GET":
-		exp := getExperimentStatus(w, orgId, repoId, expId)
-		date := time.Now().Format(http.TimeFormat)
-		log.Printf("%v\n", date)
-		log.Printf("State %v\n", exp)
-		w.Header().Set("Content-Type", "image/svg+xml")
-		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-		w.Header().Set("Date", date)
-		w.Header().Set("Expires", date)
-		switch exp.Status {
-		case "invalid":
-			w.Write(badges["invalid"])
-			break
-		case "ok":
-			w.Write(badges["ok"])
-			break
-		case "fail":
-			w.Write(badges["fail"])
-			break
-		case "gold":
-			w.Write(badges["gold"])
-			break
-		}
+	getBadge(w, orgId, repoId, expId, sha)
+}
+
+func getSpecificBadge(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	orgId := vars["orgId"]
+	repoId := vars["repoId"]
+	expId := vars["expId"]
+	sha := vars["sha"]
+
+	getBadge(w, orgId, repoId, expId, sha)
+}
+
+func getBadge(w http.ResponseWriter, orgId, repoId, expId, sha string) {
+	exp := getExperimentStatus(w, orgId, repoId, expId, sha)
+	date := time.Now().Format(http.TimeFormat)
+	log.Printf("%v\n", date)
+	log.Printf("State %v\n", exp)
+	w.Header().Set("Content-Type", "image/svg+xml")
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Date", date)
+	w.Header().Set("Expires", date)
+	switch exp {
+	case "invalid":
+		w.Write(badges["invalid"])
+		break
+	case "ok":
+		w.Write(badges["ok"])
+		break
+	case "fail":
+		w.Write(badges["fail"])
+		break
+	case "gold":
+		w.Write(badges["gold"])
+		break
 	}
 }
 
