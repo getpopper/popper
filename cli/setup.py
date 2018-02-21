c645df1794cb0397419906390743891d19980a7d
Ivo Jimenez <ivo.jimenez@gmail.com>
2018-02-20 16:37:46 -0800

    Adds skeleton for python port
    issue #193

---
+++ cli/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='popper',
+    version='0.6-dev',
+    packages=['popper', 'popper.commands'],
+    include_package_data=True,
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        popper=popper.cli:cli
+    ''',
+)
