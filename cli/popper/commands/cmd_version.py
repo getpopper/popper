e1253b48d616d347d1c7c5d86d0e417baea7c933
Ujjwal Ayyangar <ujjwal.097@gmail.com>
2018-02-28 01:33:42 +0530

    Fixes applied for single-sourcing the version

--- cli/popper/commands/cmd_version.py
+++ cli/popper/commands/cmd_version.py
@@ -1,11 +1,11 @@
 import click
 import popper.utils as pu
-import popper
-from popper.cli import pass_context
+from .. import __version__ as popper_version
+from ..cli import pass_context
 
 
 @click.command('version', short_help='Show version of Popper and exit.')
 @pass_context
 def cli(ctx):
     """Displays version of Popper and exit."""
-    pu.info(popper.__version__)
+    pu.info(popper_version)
