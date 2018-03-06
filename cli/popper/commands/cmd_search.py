01753922597ada5d4d26a106295a24341b9f518d
Ujjwal Ayyangar <ujjwal.097@gmail.com>
2018-03-06 13:40:46 +0530

    not adding

--- cli/popper/commands/cmd_search.py
+++ cli/popper/commands/cmd_search.py
@@ -3,11 +3,13 @@ import os
 import glob
 import popper.utils as pu
 from popper.cli import pass_context
+import yaml
+
 
 @click.command('search', short_help='Used to search for an experiment in your pipeline folder')
-@click.argument('experiment', required=True)
+@click.argument('repository', required=False)
 @pass_context
-def cli(ctx, experiment):
+def cli(ctx, repository):
     """Searches the pipeline folder for the specified experiment and returns the path 
     if the experiment is present.
     
@@ -18,15 +20,36 @@ def cli(ctx, experiment):
     """
 
     project_root = pu.get_project_root()
-    pipeline_path = os.path.join(project_root, 'pipelines')
 
-    search_experiments(pipeline_path, experiment)
+    popperized_path = os.path.join(project_root,'popperized')
 
-def search_experiments(pipeline_path, experiment):
-    result = glob.glob(os.path.join(pipeline_path, experiment))
-    if result:    
-        pu.info(experiment + ' found at path :- '+result[0])
+    config = pu.read_config()
+
+    if !config['popperized']:
+        pu.fail("No popperized repositories have been added")
+
+    if repository in config['popperized']:
+        repo_folder = repository.replace('/','_',-1)
+        repo_path = os.path.join(popperized_path,repo_folder)
+        get_repository(repo_path)
     else:
-        pu.fail(experiment + ' not found')
+        search_repositories(config, popperized_path)
 
+def search_repositories(config, popperized_path):
+    if config['popperized']:
+        repositories = config['popperized']
+        for repo in repositories:
+            repo_folder = repo.replace('/','_',-1)
+            repo_path = os.path.join(popperized_path,repo_folder)
+            get_repository(repo_path)
+    else:
+        pu.info("There are no repositories to check", fg="yellow", bold=True)
+
+
+def get_repository(repo_path):
+    config_filename = os.path.join(repo_path, '.popper.yml')
+    with open(config_filename, 'r') as f:
+        config = yaml.load(f.read())
+
+    pu.print_yaml(config['metadata'])
 
