c3ce64ed6ba914967554ea943cdb967b7742abf4
Ivo Jimenez <ivo.jimenez@gmail.com>
2018-03-10 13:50:10 -0800

    Revert "Adds cmd_stages.py file (#245)" (#264)
    This reverts commit a4bf4a4c89884720aa02bdac1e0f0327d19a6f98.

--- cli/popper/commands/cmd_init.py
+++ cli/popper/commands/cmd_init.py
@@ -42,7 +42,6 @@ def cli(ctx, name, stages, envs, existing):
     a folder, which is assumed to contain bash scripts. --stages must be given.
     """
     project_root = pu.get_project_root()
-    stages_list = pu.get_stages(stages)
 
     # init repo
     if name is None:
@@ -56,7 +55,7 @@ def cli(ctx, name, stages, envs, existing):
         # existing pipeline
         abs_path = os.path.join(project_root, name)
         relative_path = name
-        initialize_existing_pipeline(abs_path, stages_list, envs)
+        initialize_existing_pipeline(abs_path, stages, envs)
     elif name == 'paper':
         # create a paper pipeline
         abs_path = os.path.join(project_root, 'paper')
@@ -66,7 +65,7 @@ def cli(ctx, name, stages, envs, existing):
         # new pipeline
         abs_path = os.path.join(project_root, 'pipelines', name)
         relative_path = os.path.join('pipelines', name)
-        initialize_new_pipeline(abs_path, stages_list, envs)
+        initialize_new_pipeline(abs_path, stages, envs)
 
     pu.update_config(name, stages, envs, relative_path)
 
@@ -83,8 +82,8 @@ def initialize_repo(project_root):
     pu.info('Popperized repository ' + project_root, fg='blue', bold=True)
 
 
-def initialize_existing_pipeline(pipeline_path, stages_list, envs):
-    for s in stages_list:
+def initialize_existing_pipeline(pipeline_path, stages, envs):
+    for s in stages.split(','):
         s_filename = os.path.join(pipeline_path, s)
         if not isfile(s_filename) and not isfile(s_filename+'.sh'):
             pu.fail(
@@ -111,7 +110,7 @@ def initialize_paper(paper_path, envs):
     with open(os.path.join(paper_path, 'README',), 'w') as f:
         f.write('# ' + basename(paper_path)+ '\n')
 
-def initialize_new_pipeline(pipeline_path, stages_list, envs):
+def initialize_new_pipeline(pipeline_path, stages, envs):
 
     # create folders
     if isdir(pipeline_path) or isfile(pipeline_path):
@@ -119,7 +118,7 @@ def initialize_new_pipeline(pipeline_path, stages_list, envs):
     os.makedirs(pipeline_path)
 
     # write stage bash scripts
-    for s in stages_list:
+    for s in stages.split(','):
         if not s.endswith('.sh'):
             s = s + '.sh'
 
