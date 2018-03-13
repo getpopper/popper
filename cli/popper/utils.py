getpopper/popper

commit 4f142ab7f4576e40cf08f37354be31110d486640

Fixes #225 Add --ignore-errors flag to 'popper run' (#244)
* Add --ignore-errors flag to 'popper run'

A --ignore-errors flag has been added to the popper run command
which executes all pipelines even when there is an error in one
(when no pipeline argument is provided) and shows a warning when
used while providing an argument for pipeline.

Fixes #225.

* Create separate utility function for warnings

cli/popper/utils.py
@@ -105,13 +105,19 @@ def fail(msg):
     sys.exit(1)
 
 
+def warn(msg):
+    click.secho('WARNING: ' + msg, fg='orange', bold=True)
+
+
 def info(msg, **styles):
     """Prints the message on the terminal."""
     click.secho(msg, **styles)
 
+
 def print_yaml(msg, **styles):
     """Prints the messages in YAML's block format. """
     click.secho(yaml.dump(msg, default_flow_style = False), **styles)
+
 
 def parse_timeout(timeout):
     """Takes timeout as string and parses it to obtain the number of seconds.

cli/popper/commands/cmd_run.py
@@ -29,8 +29,15 @@ from subprocess import check_output
     help='Comma-separated list of stages to skip.',
     required=False,
 )
+@click.option(
+    '--ignore-errors',
+    is_flag=True,
+    help='Execute all pipelines even if there is a failure, '
+         'only when no pipeline argument is provided ',
+    required=False,
+)
 @pass_context
-def cli(ctx, pipeline, timeout, skip):
+def cli(ctx, pipeline, timeout, skip, ignore_errors):
     """Executes a pipeline and reports its status. When PIPELINE is given, it
     executes only the pipeline with such a name. If the argument is omitted,
     all pipelines are executed in lexicographical order. Reports an error if
@@ -46,6 +53,8 @@ def cli(ctx, pipeline, timeout, skip):
         sys.exit(0)
 
     if pipeline:
+        if ignore_errors:
+            pu.warn("ignore-errors flag is ignored when pipeline argument is provided")
         if pipeline not in pipes:
             pu.fail("Cannot find pipeline {} in .popper.yml".format(pipeline))
         status = run_pipeline(project_root, pipes[pipeline], time_out, skip)
@@ -59,7 +68,7 @@ def cli(ctx, pipeline, timeout, skip):
             for pipe in pipes:
                 status = run_pipeline(project_root, pipes[pipe], time_out, skip)
 
-                if status == 'FAIL':
+                if status == 'FAIL' and not ignore_errors:
                     break
 
     os.chdir(cwd)
@@ -117,7 +126,8 @@ def run_pipeline(project_root, pipeline, timeout, skip):
         f.write(STATUS + '\n')
 
     pu.info('status : ' + STATUS, fg='green', bold=True)
-    
+    sys.stdout.write('\n')
+
     return STATUS
 
 

commit 6b91dc7d9f48d27ced32da61dea3366a0f61de47

Change color of warning from orange to magenta
orange color used for warnings is no ANSI color and hence the
warn utility method won't function correctly. Changing 'orange' to
'magenta' does the job.

Rectifies error introduced in PR #244.

cli/popper/utils.py
@@ -118,7 +118,7 @@ def fail(msg):
 
 
 def warn(msg):
-    click.secho('WARNING: ' + msg, fg='orange', bold=True)
+    click.secho('WARNING: ' + msg, fg='magenta', bold=True)
 
 
 def info(msg, **styles):