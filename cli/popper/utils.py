c0ff5a06ffd3c944f76e0382e5d2d6e538604560
Sina Hamedian <shamedian@gmail.com>
2018-03-20 09:40:04 -0700

    change yaml.dump to yaml.safe_dump to accomodate printing unicode strings

--- cli/popper/utils.py
+++ cli/popper/utils.py
@@ -128,8 +128,7 @@ def info(msg, **styles):
 
 def print_yaml(msg, **styles):
     """Prints the messages in YAML's block format. """
-    click.secho(yaml.dump(msg, default_flow_style=False), **styles)
-
+    click.secho(yaml.safe_dump(msg, default_flow_style=False, encoding='utf-8', allow_unicode=True), **styles)
 
 def parse_timeout(timeout):
     """Takes timeout as string and parses it to obtain the number of seconds.
