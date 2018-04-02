37c0aa63a36d1af5f70c054ae6de57defc570ad7
Ankan Poddar <friendyankan@gmail.com>
2018-04-02 11:05:16 +0530

    Remove the replace flag for the stages sub-command

--- cli/popper/commands/cmd_stages.py
+++ cli/popper/commands/cmd_stages.py
@@ -5,36 +5,24 @@ import popper.utils as pu
 
 from popper.cli import pass_context
 
+
 @click.command(
     'stages', short_help='See and manipulate stages of a pipeline.'
 )
 @click.argument('pipeline', required=True)
 @click.option(
     '--set',
-    help='Provide comma separated list of stages to set the stages of a '
-         'pipeline. Without the --replace flag, the stages are added alongwith '
-         'the existing stages, otherwise they are replaced.',
-    required=False,
-)
-@click.option(
-    '--replace',
-    is_flag=True,
-    help='Use alongwith the --set option to overwrite the curent stages.',
+    help='Comma separated list of stages in the pipeline.',
     required=False,
 )
 @pass_context
-def cli(ctx, pipeline, set, replace):
+def cli(ctx, pipeline, set):
     """View or change the stages of a pipeline.
     """
     config = pu.read_config()
     if pipeline in config['pipelines']:
         if set:
-            if replace:
-                config['pipelines'][pipeline]['stages'] = set.split(',')
-            else:
-                for stage in set.split(','):
-                    if stage not in config['pipelines'][pipeline]['stages']:
-                        config['pipelines'][pipeline]['stages'].append(stage)
+            config['pipelines'][pipeline]['stages'] = set.split(',')
             pu.write_config(config)
         pu.info("\nStages:", fg="yellow")
         pu.print_yaml(config['pipelines'][pipeline]['stages'], fg="white")
