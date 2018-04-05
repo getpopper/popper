373d2bb2dadfca2fa30fc922b1a926ba7ecadde8
Ankan Poddar <friendyankan@gmail.com>
2018-04-05 23:50:38 +0530

    Fixes #229 Add a stages subcommand to view or update stages of a pipeline (#296)
    * Add a stages subcommand to view or update stages of a pipeline
    
    The stages sub-command outputs the stages of a pipeline, provided
    as an argument without the --set option. 
    
    Fixes #229.

---
+++ cli/popper/commands/cmd_stages.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import click
+import popper.utils as pu
+
+from popper.cli import pass_context
+
+
+@click.command(
+    'stages', short_help='See and manipulate stages of a pipeline.'
+)
+@click.argument('pipeline', required=True)
+@click.option(
+    '--set',
+    help='Comma separated list of stages in the pipeline.',
+    required=False,
+)
+@pass_context
+def cli(ctx, pipeline, set):
+    """View or change the stages of a pipeline.
+    """
+    config = pu.read_config()
+    if pipeline in config['pipelines']:
+        if set:
+            config['pipelines'][pipeline]['stages'] = set.split(',')
+            pu.write_config(config)
+        pu.info("\nStages:", fg="yellow")
+        pu.print_yaml(config['pipelines'][pipeline]['stages'], fg="white")
+    else:
+        pu.fail("The pipeline {} is not defined. \nSee popper.yml file to see "
+                "which pipelines are defined.".format(pipeline))
