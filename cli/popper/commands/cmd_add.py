11977ad868894e5562301b2eb78c0900ccba156c
Ujjwal Ayyangar <ujjwal.097@gmail.com>
2018-05-24 00:19:21 +0530

    Decreasing API requests for popper add (#318)
    * Decreasing API requests for popper add
    
    * pep8 linting

--- cli/popper/commands/cmd_add.py
+++ cli/popper/commands/cmd_add.py
@@ -5,6 +5,7 @@ import os
 import requests
 import popper.utils as pu
 import yaml
+import zipfile
 from io import BytesIO
 from popper.cli import pass_context
 
@@ -28,23 +29,32 @@ def cli(ctx, pipeline):
 
     project_root = pu.get_project_root()
 
-    path = os.path.join(project_root, 'pipelines')
-    if os.path.exists(path):
+    pipelines_dir = os.path.join(project_root, 'pipelines')
+    if os.path.exists(pipelines_dir):
         pass
     else:
         os.chdir(project_root)
         os.mkdir('pipelines')
 
-    dirname = pipeline_name
-    url = ('https://api.github.com/repos/{}/{}/contents/pipelines/{}'
-           .format(owner, repo, pipeline_name))
+    pipeline_path = os.path.join(pipelines_dir, pipeline_name)
+    os.chdir(pipelines_dir)
+    os.mkdir(pipeline_name)
+    os.chdir(pipeline_path)
 
+    pipeline_url = 'https://github.com/{}/{}/tree/master/pipelines/{}' \
+        .format(owner, repo, pipeline_name)
+    download_url = 'https://github-download-only-a-folder.glitch.me/dl?url={}'\
+        .format(pipeline_url)
+
+    pu.info("Downloading pipeline {} ... ".format(pipeline_name))
+    r = requests.get(download_url)
+    z = zipfile.ZipFile(BytesIO(r.content))
+    z.extractall()
+    pu.info("Updating the configuration ... ")
     repo_config = get_config(owner, repo)
+    update_config(owner, repo, pipeline_name, pipeline_path, repo_config)
 
-    save_directory(path, dirname, url)
-    path = os.path.join(path, pipeline_name)
-    update_config(owner, repo, pipeline_name, path, repo_config)
-    pu.info("Pipeline {} successfully added.".format(pipeline_name)
+    pu.info("Pipeline {} has been added successfully.".format(pipeline_name)
             + " It can be viewed in the pipelines directory.",
             fg="green")
 
@@ -63,69 +73,6 @@ def get_config(owner, repo):
     return config
 
 
-def save_file(path, filename, download_url):
-    """Helper method to save a file.
-    """
-    os.chdir(path)
-    r = requests.get(download_url)
-    if r.status_code != 200:
-        pu.fail("Could not download the file {}. Make sure the file "
-                "exists in the pipeline and try again.".format(filename))
-    with open(filename, 'wb') as f:
-        f.writelines(BytesIO(r.content))
-
-
-def save_directory(path, dirname, url, topmost=1):
-    """Recursive method to handle directory download. Creates the empty
-    directory, saves the files that belong to that directory and then calls
-    itself for other directories in that directory.
-    """
-    r = requests.get(url)
-    if r.status_code != 200:
-        pu.fail("Could not download the directory {}. Make sure the directory "
-                "exists in the pipeline and try again.".format(dirname))
-
-    os.chdir(path)
-    try:
-        os.mkdir(dirname)
-    except FileExistsError:
-        pass
-    path = os.path.join(path, dirname)
-
-    response = r.json()
-    if topmost == 1:
-        # Progressbar to show the number of files installed.
-        with click.progressbar(response,
-                               show_eta=False,
-                               label='Downloading pipeline files:',
-                               item_show_func=lambda i: ''
-                               + '| Current File/Directory > '
-                               + i['name'] if i else None,
-                               bar_template='[%(bar)s] %(label)s | %(info)s',
-                               show_percent=True) as bar:
-
-            for item in bar:
-                save_directory_util(path, dirname, url, item)
-    else:
-        for item in response:
-            save_directory_util(path, dirname, url, item)
-
-
-def save_directory_util(path, dirname, url, item):
-    """A utility function that recursively calls the save_directory
-    and save_file function as per the requirements."""
-    if item['type'] == 'file':
-        filename = item['name']
-        download_url = item['download_url']
-        filepath = item['path']
-        save_file(path, filename, download_url)
-    else:
-        dirname = item['name']
-        url = item['url']
-        dirpath = item['path']
-        save_directory(path, dirname, url, 0)
-
-
 def update_config(owner, repo, pipeline_name, path, repo_config):
     """Adds the information about the added pipeline in the
     popperized entry of the .popper.yml file.
@@ -142,7 +89,10 @@ def update_config(owner, repo, pipeline_name, path, repo_config):
             'validate.sh',
             'teardown.sh']
 
-    pipeline_envs = repo_config['envs'][pipeline_name]
+    pipeline_envs = []
+    if 'envs' in repo_config:
+        pipeline_envs = repo_config['envs'][pipeline_name]
+
     source_url = 'github.com/{}/{}'.format(owner, repo)
     config = pu.read_config()
     config['pipelines'][pipeline_name] = {
