ce73d21e515934daa3d114a1c996664ec2fb3b48
Franko1307 <franciscogonzalez1307@gmail.com>
2018-06-13 14:58:12 -0700

    logo

--- docs/conf.py
+++ docs/conf.py
@@ -158,7 +158,7 @@ html_title = u'Popper 1.0.0 documentation'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = '/figures/popper_logo.png'
+html_logo = '../docs/figures/popper_logo.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
