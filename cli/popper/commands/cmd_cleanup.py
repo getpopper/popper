f6003c34833016c409d6115ceae09cb5092c9279
Ujjwal Ayyangar <ujjwal.097@gmail.com>
2018-06-22 00:32:32 +0530

    popper cleanup fixed

--- cli/popper/commands/cmd_cleanup.py
+++ cli/popper/commands/cmd_cleanup.py
@@ -6,6 +6,11 @@ import popper.utils as pu
 
 from popper.cli import pass_context
 
+# For compatibility between python 2.x and 3.x versions
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
 
 @click.command(
     'cleanup', short_help='Synchronize your pipelines and popper.yml file.'
@@ -15,26 +20,33 @@ def cli(ctx):
     """Synchronize your pipelines and popper.yml file if any pipeline or stage
     has been deleted.
     """
-    pipeline_dir = os.path.join(pu.get_project_root(), 'pipelines')
+    #pipeline_dir = os.path.join(pu.get_project_root(), 'pipelines')
     popper_config = pu.read_config()
-    pipelines = {}
+    project_root = pu.get_project_root()
+    pipelines = popper_config['pipelines']
+    
+    for p in list(pipelines):
+        pipeline = pipelines[p]
 
-    for pipeline in os.listdir(pipeline_dir):
-        envs = popper_config['pipelines'][pipeline]['envs']
-        relative_path = popper_config['pipelines'][pipeline]['path']
-        defined_stages = popper_config['pipelines'][pipeline]['stages']
-        existing_stages = []
-        for stage in defined_stages:
-            os.chdir(os.path.join(pipeline_dir, pipeline))
-            if os.path.exists(stage+'.sh') or os.path.exists(stage):
-                existing_stages.append(stage)
-        pipelines[pipeline] = {
-            'envs': envs,
-            'path': relative_path,
-            'stages': existing_stages
-        }
+        pipe_path = os.path.join(project_root,pipeline['path'])
+        
+        if os.path.exists(pipe_path):
+
+            # Synchronizing stages
+            stages = pipeline['stages']
+            
+            for stage in stages:
+                stage_path = os.path.join(pipe_path,stage+'.sh')
+                if os.path.exists(stage_path):
+                    pass
+                else:
+                    pipeline['stages'].remove(stage)
+
+        else:
+            del pipelines[p]
 
     popper_config['pipelines'] = pipelines
+    
     pu.write_config(popper_config)
 
     pu.info("\nYour popper.yml file has been updated! Run git diff to see "
