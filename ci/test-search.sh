0d761e8058c9716e719552d92a454f9c352e7c15
Rafael Castillo <rafael@trfs.me>
2018-06-28 01:32:20 -0700

    Fix tests that expect errors

--- ci/test-search.sh
+++ ci/test-search.sh
@@ -10,19 +10,29 @@ init_test
 popper search quiho
 popper search data-science --include-readme | grep '>'
 test -d ./.cache
+
 popper search --ls
 popper search quiho --skip-update
 popper search linux --skip-update
 popper search --rm popperized
+
 set +e
 cat .popper.yml | grep 'github/popperized'
-test $? -eq 1
+if [ $? -eq 0 ];
+then
+  exit 1
+fi
 set -e
+
 popper search --add popperized
 cat .popper.yml | grep 'github/popperized'
 init_test
+
 set +e
 popper search quiho --skip-update
-test $? -eq 1
+if [ $? -eq 0 ];
+then
+  exit 1
+fi
 set -e
 
