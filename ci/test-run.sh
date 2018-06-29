f81e641a99e15c5541d980937910c6795574e01e
Francisco Enrique Cordova Gonzalez <franciscogonzalez1307@gmail.com>
2018-06-29 07:07:11 -0700

    skips test based on commit messages (#380)
    Adds ability to skip tests based on a commit message.
    
    fixes #89

--- ci/test-run.sh
+++ ci/test-run.sh
@@ -9,13 +9,13 @@ popper init mypipeone
 popper run mypipeone
 test -f pipelines/mypipeone/popper_logs/setup.sh.err
 test -f pipelines/mypipeone/popper_logs/setup.sh.out
+test -f pipelines/mypipeone/popper_logs/run.sh.err
 test -f pipelines/mypipeone/popper_logs/run.sh.out
-test -f pipelines/mypipeone/popper_logs/run.sh.out
+test -f pipelines/mypipeone/popper_logs/post-run.sh.err
 test -f pipelines/mypipeone/popper_logs/post-run.sh.out
-test -f pipelines/mypipeone/popper_logs/post-run.sh.out
+test -f pipelines/mypipeone/popper_logs/validate.sh.err
 test -f pipelines/mypipeone/popper_logs/validate.sh.out
-test -f pipelines/mypipeone/popper_logs/validate.sh.out
-test -f pipelines/mypipeone/popper_logs/teardown.sh.out
+test -f pipelines/mypipeone/popper_logs/teardown.sh.err
 test -f pipelines/mypipeone/popper_logs/teardown.sh.out
 test -f pipelines/mypipeone/popper_status
 
@@ -38,4 +38,35 @@ do
   test -f pipelines/pipetwo/popper_logs/$stage.sh.err
   test -f pipelines/pipetwo/popper_logs/$stage.sh.out
 done
+
+# test skipping based on commit
+init_test
+
+git config user.email "<>"
+git config user.name "test travis ci"
+
+popper init mypipeone
+popper init mypipetwo
+
+git commit --allow-empty -m "popper:skip this is a test"
+
+popper run
+
+test ! -f pipelines/mypipeone/popper_logs/setup.sh.err
+test ! -f pipelines/mypipeone/popper_logs/setup.sh.out
+
+test ! -f pipelines/mypipetwo/popper_logs/setup.sh.err
+test ! -f pipelines/mypipetwo/popper_logs/setup.sh.out
+
+
+git commit --allow-empty -m "popper:whitelist[mypipeone] this is a test"
+popper run
+
+test -f pipelines/mypipeone/popper_logs/setup.sh.err
+test -f pipelines/mypipeone/popper_logs/setup.sh.out
+
+test ! -f pipelines/mypipetwo/popper_logs/setup.sh.err
+test ! -f pipelines/mypipetwo/popper_logs/setup.sh.out
+
+
 
