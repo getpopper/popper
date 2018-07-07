66da8cb064a996d60354853356aa2afe48a93c8b
Ujjwal Ayyangar <ujjwal.097@gmail.com>
2018-07-07 17:11:40 +0530

    fix tests

--- ci/test-reset.sh
+++ ci/test-reset.sh
@@ -7,15 +7,17 @@ source common-setup.sh
 init_test
 popper init myp
 set +e
-printf "y\n" | popper reset
+echo -e "yes" | popper reset
 cat .popper.yml | grep 'myp'
 if [ $? -eq 0 ];
 then
   exit 1
 fi
-set -e
 
 init_test
 popper init myp
-printf "n\n" | popper reset
+echo -e "no" | popper reset
+set -e
 cat .popper.yml | grep 'myp'
+
+
