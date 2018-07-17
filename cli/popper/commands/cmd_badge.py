f03df8c2fdbd64ab2220fa799dfeeac9439ffbbb
Ankan Poddar <friendyankan@gmail.com>
2018-07-17 10:59:34 +0530

    Fixes #141 Add --inplace flag for popper badge command (#404)
      * Fixes #141. Add a --inplace flag for popper badge command
      * Add tests for --history and --inplace flags for popper badge command

--- cli/popper/commands/cmd_badge.py
+++ cli/popper/commands/cmd_badge.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import click
 import requests
 import popper.utils as pu
@@ -43,16 +44,26 @@ services = {
 )
 @click.option(
     '--history',
-    help='Get the history of badges generated for popper',
+    help=(
+        'Get the history of badges generated for popper. '
+        'Can\'t be used when --service is provided'
+    ),
+    required=False,
+    is_flag=True
+)
+@click.option(
+    '--inplace',
+    help=(
+        'Write markup for badge to the README. '
+        'Can\'t be used when --history is provided'
+    ),
     required=False,
     is_flag=True
 )
 @pass_context
-def cli(ctx, service, history):
+def cli(ctx, service, history, inplace):
     """Generates markdown for the badge of a service. Currently available
-    services are: CloudLab, Chameleon, Google Cloud Engine and Popper. Both
-    --service and --history cannot be used together, but one of them
-    is required.
+    services are: CloudLab, Chameleon, Google Cloud Engine and Popper.
     """
     if (history and service) or (not history and not service):
         raise BadArgumentUsage(
@@ -83,24 +94,40 @@ def cli(ctx, service, history):
         else:
             markup = '[![{}]({})]({})'.format(*services[service])
 
-        name, _, _ = services[service]
-        pu.info('To add the "{}" badge to your readme, put this markdown '
-                'inside your readme file:\n'.format(name))
-        pu.info(markup)
+        if inplace:
+            try:
+                os.chdir(pu.get_project_root())
+                with open('README.md', 'r+') as f:
+                    content = f.read()
+                    f.seek(0, 0)
+                    f.write(markup + '\n\n' + content)
+            except FileNotFoundError:
+                pu.fail("README.md does not exist at the root of the project")
+        else:
+            name, _, _ = services[service]
+            pu.info('To add the "{}" badge to your readme, put this markdown '
+                    'inside your readme file:\n'.format(name))
+            pu.info(markup)
 
     else:
-        baseurl = pu.read_config().get(
-            'badge-server-url', 'http://badges.falsifiable.us'
-        )
-        remote_url = pu.get_remote_url()
-        if not remote_url:
-            pu.fail("Failed to fetch remote url for git repository")
-        org, repo = remote_url.split('/')[-2:]
-        try:
-            r = requests.get('{}/{}/{}/list'.format(baseurl, org, repo))
-            if len(r.json()) == 0:
-                pu.info("No records to show")
-            else:
-                pu.print_yaml(r.json())
-        except requests.exceptions.RequestException:
-            pu.fail("Could not communicate with the badge server")
+        if not inplace:
+            baseurl = pu.read_config().get(
+                'badge-server-url', 'http://badges.falsifiable.us'
+            )
+            remote_url = pu.get_remote_url()
+            if not remote_url:
+                pu.fail("Failed to fetch remote url for git repository")
+            org, repo = remote_url.split('/')[-2:]
+            try:
+                r = requests.get('{}/{}/{}/list'.format(baseurl, org, repo))
+                if len(r.json()) == 0:
+                    pu.info("No records to show")
+                else:
+                    pu.print_yaml(r.json())
+            except requests.exceptions.RequestException:
+                pu.fail("Could not communicate with the badge server")
+        else:
+            raise BadArgumentUsage(
+                "--history and --inplace can't be used together. \n"
+                "See popper badge --help for more info."
+            )
