481e5b56d1076cfeb973d2350b419010d577f716
Ankan Poddar <friendyankan@gmail.com>
2018-07-23 14:54:31 +0530

    Modify the tests

--- ci/test-add.sh
+++ ci/test-add.sh
@@ -15,16 +15,25 @@ test -f pipelines/co2-emissions/setup.sh
 test -f pipelines/co2-emissions/validate.sh
 
 init_test
-popper add popperized/swc-lesson-pipelines/co2-emissions
-test -d pipelines/co2-emissions
-test -f pipelines/co2-emissions/README.md
-popper rm co2-emissions
+popper add popperized/swc-lesson-pipelines/co2-emissions foo/emissions
+test -d foo/emissions
+cat .popper.yml | grep 'emissions:'
+cat .popper.yml | grep 'path: foo/emissions'
+test -f foo/emissions/run.sh
 
 init_test
-popper add popperized/swc-lesson-pipelines/co2-emissions swc-lesson-pipelines
-test -d pipelines/swc-lesson-pipelines/co2-emissions
-test -f pipelines/swc-lesson-pipelines/co2-emissions/README.md
-popper rm co2-emissions
+popper add popperized/swc-lesson-pipelines/co2-emissions foo
+test -d pipelines/foo
+cat .popper.yml | grep 'foo:'
+cat .popper.yml | grep 'path: pipelines/foo'
+test -f pipelines/foo/run.sh
+
+init_test
+popper add popperized/swc-lesson-pipelines/co2-emissions foo/
+test -d foo/
+cat .popper.yml | grep 'foo:'
+cat .popper.yml | grep 'path: foo'
+test -f foo/run.sh
 
 # info command
 popper info popperized/popper-readthedocs-examples/docker-data-science | grep 'url'
