37df5a0f43b6c679258823e100b791a76f7323ed
Ankan Poddar <friendyankan@gmail.com>
2018-07-26 20:03:49 +0530

    Fixes #428. Create the virtualenv with python3 in setup.py

--- ci/setup.sh
+++ ci/setup.sh
@@ -5,7 +5,7 @@ if [ $USE_VIRTUALENV ];
 then
   rm -rf /tmp/popper-env
   echo "Creating temporary virtualenv"
-  virtualenv /tmp/popper-env
+  virtualenv /tmp/popper-env --python=python3
   source /tmp/popper-env/bin/activate
 
   cd ../cli
