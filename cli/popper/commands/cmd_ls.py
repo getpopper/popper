d335951bef1a14ee55080f561d90c935da502867
Ujjwal Ayyangar <ujjwal.097@gmail.com>
2018-07-31 00:36:25 +0530

    popper ls command added

---
+++ cli/popper/commands/cmd_ls.py
@@ -0,0 +1,32 @@
+import os
+import click
+import shutil
+import popper.utils as pu
+from popper.cli import pass_context
+
+
+@click.command(
+    'ls',
+    short_help='Shows the available pipelines in a repository')
+@pass_context
+def cli(ctx):
+    """Used to list down the avaliable pipelines
+    in a popper repository
+
+    Example:
+
+    > popper ls
+     - experiment-1
+     - experiment-2
+
+    """
+
+    pipelines = list(pu.read_config()['pipelines'].keys())
+    if 'paper' in pipelines:
+        pipelines.remove('paper')
+
+    if len(pipelines) == 0:
+        pu.info("There are no pipelines in this repository", fg="red")
+    else:
+        pu.info("The available pipelines are :\n", fg="cyan")
+        pu.print_yaml(pipelines, fg="cyan")
