6f79c214de14c00632976335df117c8a68943ac7
Rafael Castillo <rafael@trfs.me>
2018-08-02 04:01:03 -0700

    Fix regression for skipping whole pipelines (#443)
    Fixes popper run --skip regression
    
    fixes #442

--- ci/common-setup.sh
+++ ci/common-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -ex
+
+repo_root=/tmp/mypaper
 
 function init_test {
   cd
@@ -15,6 +16,30 @@ function init_test {
   popper init
 }
 
+if [ -f /.dockerenv ]; then
+  output_dir=popper
+else
+  output_dir=popper/host
+fi
+
+pipe_ran() {
+  test -d $repo_root/pipelines/$1/$output_dir
+}
+
+pipe_didnt_run() {
+  test ! -d $repo_root/pipelines/$1/$output_dir
+}
+
+stage_ran() {
+  test -f $repo_root/pipelines/$1/$output_dir/$2.sh.out
+  test -f $repo_root/pipelines/$1/$output_dir/$2.sh.err
+}
+
+stage_didnt_run() {
+  test ! -f $repo_root/pipelines/$1/$output_dir/$2.sh.out
+  test ! -f $repo_root/pipelines/$1/$output_dir/$2.sh.err
+}
+
 if [ $USE_VIRTUALENV ];
 then
   source /tmp/popper-env/bin/activate
@@ -23,5 +48,5 @@ fi
 export PATH=$PATH:$PWD/cli/bin
 export PYTHONUNBUFFERED=1
 
-which popper
+echo Testing with popper binary located at $( which popper )
 
--- ci/test-run.sh
+++ ci/test-run.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-set -ex
-
 source common-setup.sh
 
-if [ -f /.dockerenv ]; then
-  output_dir=popper
-else
-  output_dir=popper/host
-fi
+set -ex
 
 # run
 init_test
@@ -20,6 +14,18 @@ for s in setup run post-run validate teardown; do
 done
 test -f pipelines/mypipeone/$output_dir/popper_status
 
+
+# test skipping pipelines
+init_test
+popper init pipeone --stages=one
+popper init pipetwo --stages=two,three
+
+popper run --skip=pipeone
+
+pipe_didnt_run pipeone
+pipe_ran pipetwo
+
+
 # test skipping stages
 init_test
 popper init pipeone --stages=one,two,three,four
@@ -28,15 +34,34 @@ popper run pipeone --skip=one,two
 
 for stage in one two
 do
-  test ! -f pipelines/pipeone/$output_dir/$stage.sh.err
-  test ! -f pipelines/pipeone/$output_dir/$stage.sh.out
+  stage_didnt_run pipeone $stage
 done
 
 for stage in three four
 do
-  test -f pipelines/pipeone/$output_dir/$stage.sh.err
-  test -f pipelines/pipeone/$output_dir/$stage.sh.out
+  stage_ran pipeone $stage
 done
+
+
+# test skippint with pipeline:stage pairs
+init_test
+popper init pipeone --stages=one,two,three,four
+popper init pipetwo --stages=two,three
+
+popper run --skip=pipeone:one,pipeone:two
+
+for stage in one two
+do
+  stage_didnt_run pipeone $stage
+done
+
+for stage in three four
+do
+  stage_ran pipeone $stage
+done
+
+pipe_ran pipetwo
+
 
 # test skipping based on commit
 init_test
@@ -55,55 +80,40 @@ popper init --stages=setup mypipethree
 
 git add .
 git commit --allow-empty -m "popper:whitelist[mypipeone] this is a test"
-
 popper run --no-badge-update
 
-test -f pipelines/mypipeone/$output_dir/setup.sh.err
-test -f pipelines/mypipeone/$output_dir/setup.sh.out
-test ! -f pipelines/mypipetwo/$output_dir/setup.sh.err
-test ! -f pipelines/mypipetwo/$output_dir/setup.sh.out
-test ! -f pipelines/mypipethree/$output_dir/setup.sh.err
-test ! -f pipelines/mypipethree/$output_dir/setup.sh.out
+stage_ran mypipeone setup
+stage_didnt_run mypipetwo setup
+stage_didnt_run mypipethree setup
+
 
 git clean -dxf
-
 git commit --allow-empty -m "popper:whitelist[mypipeone,mypipetwo] this is a test"
-
 popper run --no-badge-update
 
-test -f pipelines/mypipeone/$output_dir/setup.sh.err
-test -f pipelines/mypipeone/$output_dir/setup.sh.out
-test -f pipelines/mypipetwo/$output_dir/setup.sh.err
-test -f pipelines/mypipetwo/$output_dir/setup.sh.out
-test ! -f pipelines/mypipethree/$output_dir/setup.sh.err
-test ! -f pipelines/mypipethree/$output_dir/setup.sh.out
+stage_ran mypipeone setup
+stage_ran mypipetwo setup
+stage_didnt_run mypipethree setup
+
 
 git clean -dxf
-
 git commit --allow-empty -m "popper:skip[mypipeone] this is a test"
-
 popper run --no-badge-update
 
-test ! -f pipelines/mypipeone/$output_dir/setup.sh.err
-test ! -f pipelines/mypipeone/$output_dir/setup.sh.out
-test -f pipelines/mypipetwo/$output_dir/setup.sh.err
-test -f pipelines/mypipetwo/$output_dir/setup.sh.out
-test -f pipelines/mypipethree/$output_dir/setup.sh.err
-test -f pipelines/mypipethree/$output_dir/setup.sh.out
+stage_didnt_run mypipeone setup
+stage_ran mypipetwo setup
+stage_ran mypipethree setup
+
 
 git clean -dxf
-
 git commit --allow-empty -m "popper:skip[mypipeone,mypipetwo] this is a test"
-
 popper run --no-badge-update
 
-test ! -f pipelines/mypipeone/$output_dir/setup.sh.err
-test ! -f pipelines/mypipeone/$output_dir/setup.sh.out
-test ! -f pipelines/mypipetwo/$output_dir/setup.sh.err
-test ! -f pipelines/mypipetwo/$output_dir/setup.sh.out
-test -f pipelines/mypipethree/$output_dir/setup.sh.err
-test -f pipelines/mypipethree/$output_dir/setup.sh.out
+stage_didnt_run mypipeone setup
+stage_didnt_run mypipetwo setup
+stage_ran mypipethree setup
 
-if [ $ci_not_set ]; then
+
+if [ -z $ci_not_set ]; then
   unset CI
 fi
