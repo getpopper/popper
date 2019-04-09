7f0440dc279f8cad04897c694fc20051db46f98a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-04-09 12:11:13 +0530

    implements popper add (#570)
    Adds 'popper add' subcommand for importing existing workflows to a project.
    
    fixes #564

---
+++ cli/popper/commands/cmd_add.py
@@ -0,0 +1,23 @@
+import click
+import os
+import popper.utils as pu
+import popper.scm as scm
+from popper.gha import Workflow
+from popper.cli import pass_context
+
+
+@click.command('add', short_help='Import workflow from remote repo.')
+@click.argument('path', required=True)
+@pass_context
+def cli(ctx, path):
+    """Imports a workflow from a remote project to the current project
+    directory.
+    """
+    project_root = scm.get_root_folder()
+    if not pu.is_popperized(project_root):
+        pu.fail('Repository has not been popperized.')
+        return
+    try:
+        Workflow.import_from_repo(path, project_root)
+    except Exception as e:
+        pu.fail('Failed to import from {} !'.format(path))
