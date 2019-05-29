ca77aa04a1d6bd42a66acf84a7136f0b9ff67902
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-05-29 23:34:24 +0530

    improves syntax validation error messages (#662)
    fixes #657

--- cli/popper/parser.py
+++ cli/popper/parser.py
@@ -155,7 +155,8 @@ class Workflow(object):
             wf_block = list(self._workflow['workflow'].values())[0]
             for key in wf_block.keys():
                 if key not in VALID_WORKFLOW_ATTRS:
-                    log.fail('Invalid attrs found.')
+                    log.fail(
+                        'Invalid workflow attribute \'{}\' was found.'.format(key))
             if not wf_block.get('resolves', None):
                 log.fail('[resolves] attribute must be present.')
 
@@ -168,7 +169,9 @@ class Workflow(object):
             for _, a_block in self._workflow['action'].items():
                 for key in a_block.keys():
                     if key not in VALID_ACTION_ATTRS:
-                        log.fail('Invalid attrs found.')
+                        log.fail(
+                            'Invalid action attribute \'{}\' was '
+                            'found.'.format(key))
                 if not a_block.get('uses', None):
                     log.fail('[uses] attribute must be present.')
 
