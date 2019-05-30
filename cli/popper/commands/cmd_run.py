1b7c1252730f2c9b5907bbddc21123cdc651012a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-05-30 19:45:11 +0530

    Error handling improved

--- cli/popper/commands/cmd_run.py
+++ cli/popper/commands/cmd_run.py
@@ -164,7 +164,8 @@ def workflows_from_commit_message(workflows):
 
     if 'Merge' in msg:
         log.info("Merge detected. Reading message from merged commit.")
-        msg = head_commit.parents[0].message
+        if len(head_commit.parents) == 2:
+            msg = head_commit.parents[0].message
 
     if 'popper:skip[' in msg:
         log.info("Found 'popper:skip' keyword.")
