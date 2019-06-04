bd6e8b2c1251e2d92bfa8c2ee77c27e1dd1933a7
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-06-04 23:16:17 +0530

    Implements search and info subcommand

---
+++ cli/popper/commands/cmd_info.py
@@ -0,0 +1,28 @@
+import sys
+
+import click
+
+from popper import utils as pu
+from popper.cli import log, pass_context
+
+
+@click.argument('action', required=True)
+@click.option(
+    '--update-cache',
+    help='Update the action metadata cache before looking for info.',
+    is_flag=True
+)
+@click.command('info', short_help='Show details about an action.')
+@pass_context
+def cli(ctx, action, update_cache):
+
+    metadata = pu.fetch_metadata(update_cache)
+    action_metadata = metadata.get(action, None)
+
+    if not action_metadata:
+        log.fail('Unable to find metadata for given pipeline.')
+
+    if not action_metadata['repo_readme']:
+        log.fail('This repository does not have a README.md file.')
+
+    log.info(action_metadata['repo_readme'])
---
+++ cli/popper/commands/cmd_search.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+import click
+
+from popper import utils as pu
+from popper.cli import log, pass_context
+
+
+@click.argument(
+    'keywords',
+    required=True
+)
+@click.option(
+    '--update-cache',
+    help=('Update the action metadata cache before searching.'),
+    is_flag=True
+)
+@click.option(
+    '--include-readme',
+    help=('Include README while searching for matching actions.'),
+    is_flag=True
+)
+@click.command('search', short_help='Search for actions on Github.')
+@pass_context
+def cli(ctx, keywords, update_cache, include_readme):
+    metadata = pu.fetch_metadata(update_cache)
+    result = search(metadata, keywords, include_readme)
+    log.info('Search Results : \n')
+    if result:
+        for action in result:
+            log.info('> {}\n'.format(action))
+    else:
+        log.info('No matching actions found.\n')
+
+
+def search(metadata, keyword, include_readme):
+    """Search for the keyword in the repo metadata.
+
+    Args:
+        metadata (dict) : The dict on which to run search.
+        keyword (str) : The keyword to search for.
+        include_readme (bool) : Flag variable indicating whether
+        to search keyword inside the README.md or not.
+
+    Returns:
+        list : List of repo names which confirm with
+        the search.
+    """
+    keyword = keyword.lower()
+    result = list()
+    for action, action_metadata in metadata.items():
+        if keyword in action.lower():
+            result.append(action)
+        elif include_readme:
+            if keyword in action_metadata['repo_readme'].lower():
+                result.append(action)
+    return result
