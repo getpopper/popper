c386867ac8e2640a1cf6a33e0ce1c3639cac243b
JayjeetAtGithub <jc.github@rediffmail.com>
2019-07-17 11:23:36 +0530

    Refactored test_scm

--- cli/test/test_scm.py
+++ cli/test/test_scm.py
@@ -9,11 +9,17 @@ from popper import scm
 from popper.cli import log
 
 
-class TestScmWithGit(unittest.TestCase):
+class TestScm(unittest.TestCase):
     """Unit tests for popper.scm module in git
     environment."""
 
     def setUp(self):
+        
+        if os.environ.get('POPPER_TEST_MODE') == 'with-git':
+            self.with_git = True
+        else:
+            self.with_git = False
+
         log.setLevel('CRITICAL')
         if os.path.exists('/tmp/test_folder'):
             shutil.rmtree('/tmp/test_folder')
@@ -25,6 +31,8 @@ class TestScmWithGit(unittest.TestCase):
             'github-actions-demo',
             os.path.join(os.getcwd(), 'github-actions-demo')
         )
+        if not self.with_git:
+            shutil.rmtree('/tmp/test_folder/github-actions-demo/.git')
         os.chdir('/tmp/test_folder/github-actions-demo')
 
     def tearDown(self):
@@ -44,37 +52,55 @@ class TestScmWithGit(unittest.TestCase):
 
     def test_get_user(self):
         user = scm.get_user()
-        self.assertEqual(user, 'cplee')
+        if self.with_git:
+            self.assertEqual(user, 'cplee')
+        else:
+            self.assertEqual(user, '')
 
     def test_get_remote_url(self):
         url = scm.get_remote_url()
-        self.assertEqual(url, 'https://github.com/cplee/github-actions-demo')
+        if self.with_git:
+            self.assertEqual(url, 'https://github.com/cplee/github-actions-demo')
+        else:
+            self.assertEqual(url, '')
 
     def test_get_ref(self):
         ref = scm.get_ref()
-        self.assertEqual(ref, 'refs/heads/master')
+        if self.with_git:
+            self.assertEqual(ref, 'refs/heads/master')
+        else:
+            self.assertEqual(ref, 'unknown')
 
     def test_get_sha(self):
         sha = scm.get_sha()
-        self.assertEqual(sha, 'c3c8022')
+        if self.with_git:
+            self.assertEqual(sha, 'c3c8022')
+        else:
+            self.assertEqual(sha, 'unknown')
 
     def test_get_head_commit(self):
         head_commit_object = scm.get_head_commit()
-        hexsha = head_commit_object.hexsha
-        self.assertEqual(hexsha, 'c3c8022de1513425aebbf4d98ea7b63f00a50da6')
+        if self.with_git:
+            hexsha = head_commit_object.hexsha
+            self.assertEqual(hexsha, 'c3c8022de1513425aebbf4d98ea7b63f00a50da6')
+        else:
+            self.assertIsNone(head_commit_object)
 
     def test_get_git_files(self):
         files = scm.get_git_files()
-        self.assertListEqual(files, [
-            '.github/actions/jshint/Dockerfile',
-            '.github/main.workflow',
-            '.gitignore',
-            'LICENSE',
-            'README.md',
-            'index.js',
-            'package-lock.json',
-            'package.json', 'tests/test-app.js'
-        ])
+        if self.with_git:
+            self.assertListEqual(files, [
+                '.github/actions/jshint/Dockerfile',
+                '.github/main.workflow',
+                '.gitignore',
+                'LICENSE',
+                'README.md',
+                'index.js',
+                'package-lock.json',
+                'package.json', 'tests/test-app.js'
+            ])
+        else:
+            self.assertIsNone(files)
 
     def test_clone(self):
         os.makedirs('/tmp/test_folder/test_clone')
@@ -142,137 +168,3 @@ class TestScmWithGit(unittest.TestCase):
             'github-actions-demo',
             'path/to/action',
             'develop'))
-
-
-class TestScmWithoutGit(unittest.TestCase):
-    """Unit tests for popper.scm module in non git
-    environment."""
-
-    def setUp(self):
-        log.setLevel('CRITICAL')
-        if os.path.exists('/tmp/test_folder'):
-            shutil.rmtree('/tmp/test_folder')
-        os.makedirs('/tmp/test_folder')
-        os.chdir('/tmp/test_folder')
-        scm.clone(
-            'https://github.com',
-            'cplee',
-            'github-actions-demo',
-            os.path.join(os.getcwd(), 'github-actions-demo')
-        )
-        shutil.rmtree('/tmp/test_folder/github-actions-demo/.git')
-        os.chdir('/tmp/test_folder/github-actions-demo')
-
-    def tearDown(self):
-        log.setLevel('NOTSET')
-        os.chdir('/tmp')
-        shutil.rmtree('/tmp/test_folder')
-
-    def test_get_git_root_folder(self):
-        root_folder = scm.get_git_root_folder()
-        self.assertEqual(
-            root_folder,
-            '/tmp/test_folder/github-actions-demo')
-
-    def test_get_name(self):
-        name = scm.get_name()
-        self.assertEqual(name, 'github-actions-demo')
-
-    def test_get_user(self):
-        user = scm.get_user()
-        self.assertEqual(user, "")
-
-    def test_get_remote_url(self):
-        url = scm.get_remote_url()
-        self.assertEqual(url, "")
-
-    def test_get_ref(self):
-        ref = scm.get_ref()
-        self.assertEqual(ref, 'unknown')
-
-    def test_get_sha(self):
-        sha = scm.get_sha()
-        self.assertEqual(sha, 'unknown')
-
-    def test_get_head_commit(self):
-        head_commit_object = scm.get_head_commit()
-        self.assertIsNone(head_commit_object)
-
-    def test_get_git_files(self):
-        files = scm.get_git_files()
-        self.assertIsNone(files)
-
-    def test_clone(self):
-        os.makedirs('/tmp/test_folder/test_clone')
-        os.chdir('/tmp/test_folder/test_clone')
-        scm.clone(
-            'https://github.com',
-            'JayjeetAtGithub',
-            'github-actions-demo',
-            os.path.join(os.getcwd(), 'gad'),
-            'develop'
-        )
-        repo = git.Repo(os.path.join(os.getcwd(), 'gad'))
-        self.assertEqual(repo.active_branch.name, 'develop')
-        scm.clone(
-            'https://github.com',
-            'JayjeetAtGithub',
-            'github-actions-demo',
-            os.path.join(os.getcwd(), 'gad'),
-            'master'
-        )
-        repo = git.Repo(os.path.join(os.getcwd(), 'gad'))
-        self.assertEqual(repo.active_branch.name, 'master')
-
-    def test_parse(self):
-        test_url = "ssh://git@github.com:cplee/github-actions-demo.git"
-        self.assertRaises(SystemExit, scm.parse, test_url)
-        test_url = "https://github.com/cplee@master"
-        self.assertRaises(SystemExit, scm.parse, test_url)
-        test_url = "github.com"
-        self.assertRaises(SystemExit, scm.parse, test_url)
-        test_url = "@master"
-        self.assertRaises(SystemExit, scm.parse, test_url)
-        test_url = "http://gitlab.com/cplee/github-actions-demo.git"
-        parts = scm.parse(test_url)
-        self.assertTupleEqual(parts, (
-            'http://gitlab.com',
-            'gitlab.com',
-            'cplee',
-            'github-actions-demo',
-            '', None))
-        test_url = "https://github.com/cplee/github-actions-demo@master"
-        parts = scm.parse(test_url)
-        self.assertTupleEqual(parts, (
-            'https://github.com',
-            'github.com',
-            'cplee',
-            'github-actions-demo',
-            '',
-            'master'))
-        test_url = "github.com/cplee/github-actions-demo"
-        parts = scm.parse(test_url)
-        self.assertTupleEqual(parts, (
-            'https://github.com',
-            'github.com',
-            'cplee',
-            'github-actions-demo',
-            '',
-            None))
-        test_url = "cplee/github-actions-demo/path/to/action@develop"
-        parts = scm.parse(test_url)
-        self.assertTupleEqual(parts, (
-            'https://github.com',
-            'github.com',
-            'cplee',
-            'github-actions-demo',
-            'path/to/action',
-            'develop'))
-
-
-if __name__ == "__main__":
-    test_suite = unittest.TestSuite()
-    test_suite.addTest(unittest.makeSuite(TestScmWithGit))
-    test_suite.addTest(unittest.makeSuite(TestScmWithoutGit))
-    runner = unittest.TextTestRunner()
-    runner.run(test_suite)
