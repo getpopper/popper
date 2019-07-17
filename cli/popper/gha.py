23db1cc87030372805da3aed538edb3e664f1f87
JayjeetAtGithub <jc.github@rediffmail.com>
2019-07-17 14:17:48 +0530

    Extracts env setting logic.

--- cli/popper/gha.py
+++ cli/popper/gha.py
@@ -251,6 +251,20 @@ class ActionRunner(object):
             f = open(env['GITHUB_EVENT_PATH'], 'w')
             f.close()
 
+    def prepare_environment_vars(self):
+        env = self.action.get('env', {})
+
+        for s in self.action.get('secrets', []):
+            env.update({s: os.environ[s]})
+
+        for e, v in self.env.items():
+            env.update({e: v})
+
+        env['GITHUB_ACTION'] = self.action['name']
+        env['POPPER_ACTION'] = self.action['name']
+        
+        return env
+
     def run(self, reuse=False):
         raise NotImplementedError(
             "This method is required to be implemented in derived classes."
@@ -363,18 +377,9 @@ class DockerRunner(ActionRunner):
         ))
         if self.dry_run:
             return
-
-        env = self.action.get('env', {})
-
-        for s in self.action.get('secrets', []):
-            env.update({s: os.environ.get(s)})
-
-        for e, v in self.env.items():
-            env.update({e: v})
-
-        env['GITHUB_ACTION'] = self.action['name']
-        env['POPPER_ACTION'] = self.action['name']
-
+        
+        env = self.prepare_environment_vars()
+        
         volumes = [
             '/var/run/docker.sock:/var/run/docker.sock',
             '{}:{}'.format(env['HOME'], env['HOME']),
@@ -566,17 +571,7 @@ class SingularityRunner(ActionRunner):
         """Starts the container to execute commands or run the runscript
         with the supplied args inside the container.
         """
-        env = self.action.get('env', {})
-
-        for s in self.action.get('secrets', []):
-            env.update({s: os.environ[s]})
-
-        for e, v in self.env.items():
-            env.update({e: v})
-
-        env['GITHUB_ACTION'] = self.action['name']
-        env['POPPER_ACTION'] = self.action['name']
-
+        env = self.prepare_environment_vars()
         for k, v in env.items():
             os.environ[k] = v
 
@@ -619,7 +614,9 @@ class SingularityRunner(ActionRunner):
                 ecode = ex.returncode
         else:
             ecode = 0
-
+        
+        for k, v in env.items():
+            os.environ.pop(k)
         return ecode
 
 
@@ -660,7 +657,9 @@ class HostRunner(ActionRunner):
                     os.chdir(os.path.join(root, self.action['uses']))
                     cmd[0] = os.path.join(root, self.action['uses'], cmd[0])
 
-        os.environ.update(self.action.get('env', {}))
+        env = self.prepare_environment_vars()
+        for k, v in env.items():
+            self.environ[k] = v
 
         log.info('{}[{}] {}'.format(self.msg_prefix, self.action['name'],
                                     ' '.join(cmd)))
@@ -695,8 +694,8 @@ class HostRunner(ActionRunner):
             log.action_info()
 
         # remove variables that we added to the environment
-        for i in self.action.get('env', {}):
-            os.environ.pop(i)
+        for k, v in env.items():
+            os.environ.pop(k)
 
         os.chdir(self.cwd)
 
