e84f1bdf7622bf60cd4d52ba8b9a981d72c3d492
sarthak0906 <sarthak0906@gmail.com>
2019-12-02 19:15:17 +0530

    tring to add new formatter to the log object

--- cli/popper/commands/cmd_version.py
+++ cli/popper/commands/cmd_version.py
@@ -1,11 +1,20 @@
 import click
+import logging
 
 from popper import __version__ as popper_version
 from popper.cli import pass_context, log
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+
+log.addHandler(ch)
 
 
 @click.command('version', short_help='Show version of Popper and exit.')
 @pass_context
 def cli(ctx):
     """Displays version of Popper and exit."""
-    log.info('Popper version {}'.format(popper_version))
+    log.fail('Popper version {}'.format(popper_version))
