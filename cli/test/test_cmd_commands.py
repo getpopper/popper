5fba87728dfad011a0b535a5a8d6e53a0bebcc89
Vipul Chhabra <vipulchhabra555@gmail.com>
2020-02-06 18:32:39 +0530

    Click test added for all commands

--- cli/test/test_cmd_commands.py
+++ cli/test/test_cmd_commands.py
@@ -1,6 +1,11 @@
 from click.testing import CliRunner
 import popper.commands.cmd_ci as ci
 import popper.commands.cmd_dot as dot
+import popper.commands.cmd_info as info
+import popper.commands.cmd_run as run
+import popper.commands.cmd_scaffold as scaffold
+import popper.commands.cmd_search as search
+import popper.commands.cmd_version as version
 from pathlib import Path
 import unittest
 
@@ -15,4 +20,31 @@ class TestCommands(unittest.TestCase):
         runner = CliRunner()
         p = str(Path(__file__).parents[2])+'/ci/cli.workflow'
         result = runner.invoke(dot.cli, ['--wfile', p])
+        assert result.exit_code == 0
+
+    def test_run(self):
+        runner = CliRunner()
+        p = str(Path(__file__).parents[2])+'/ci/cli.workflow'
+        result = runner.invoke(run.cli, ['--wfile', p])
+        print(result)
+        assert result.exit_code == 0
+
+    def test_info(self):
+        runner = CliRunner()
+        result = runner.invoke(info.cli, ['popperized/npm'])
+        assert result.exit_code == 0
+
+    def test_scaffold(self):
+        runner = CliRunner()
+        result = runner.invoke(scaffold.cli)
+        assert result.exit_code == 0
+
+    def test_search(self):
+        runner = CliRunner()
+        result = runner.invoke(search.cli, ['popperized/npm'])
+        assert result.exit_code == 0
+
+    def test_version(self):
+        runner = CliRunner()
+        result = runner.invoke(version.cli)
         assert result.exit_code == 0
