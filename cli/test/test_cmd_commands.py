003677bb9bf7685ef64ad7015b1f5428f1d63fae
Vipul Chhabra <vipulchhabra555@gmail.com>
2020-02-06 08:41:49 +0530

    PEP8 Fixes

--- cli/test/test_cmd_commands.py
+++ cli/test/test_cmd_commands.py
@@ -6,13 +6,13 @@ import unittest
 
 
 class TestCommands(unittest.TestCase):
-	def test_ci(self):
-		runner = CliRunner()
-		result = runner.invoke(ci.cli, ['travis'])
-		assert result.exit_code == 0
+    def test_ci(self):
+        runner = CliRunner()
+        result = runner.invoke(ci.cli, ['travis'])
+        assert result.exit_code == 0
 
-	def test_dot(self):
-		runner = CliRunner()
-		p = str(Path(__file__).parents[2])+'/ci/cli.workflow'
-		result = runner.invoke(dot.cli,['--wfile',p])
-		assert result.exit_code == 0
+    def test_dot(self):
+        runner = CliRunner()
+        p = str(Path(__file__).parents[2])+'/ci/cli.workflow'
+        result = runner.invoke(dot.cli, ['--wfile', p])
+        assert result.exit_code == 0
