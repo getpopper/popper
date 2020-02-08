b12e5073166341ca614fea80f442a64aeb9371c7
Vipul Chhabra <vipulchhabra555@gmail.com>
2020-02-08 23:31:09 +0530

    Updated as per review

--- cli/test/test_cmd_commands.py
+++ cli/test/test_cmd_commands.py
@@ -1,50 +1,99 @@
 from click.testing import CliRunner
-import popper.commands.cmd_ci as ci
-import popper.commands.cmd_dot as dot
-import popper.commands.cmd_info as info
-import popper.commands.cmd_run as run
-import popper.commands.cmd_scaffold as scaffold
-import popper.commands.cmd_search as search
 import popper.commands.cmd_version as version
+import popper.commands.cmd_run as run
+import popper.utils as pu
 from pathlib import Path
 import unittest
+import os
+from popper.cli import log
+import shutil
 
 
-class TestCommands(unittest.TestCase):
-    def test_ci(self):
-        runner = CliRunner()
-        result = runner.invoke(ci.cli, ['travis'])
-        assert result.exit_code == 0
 
-    def test_dot(self):
-        runner = CliRunner()
-        p = str(Path(__file__).parents[2])+'/ci/cli.workflow'
-        result = runner.invoke(dot.cli, ['--wfile', p])
-        assert result.exit_code == 0
-
-    def test_run(self):
-        runner = CliRunner()
-        p = str(Path(__file__).parents[2])+'/ci/cli.workflow'
-        result = runner.invoke(run.cli, ['--wfile', p])
-        print(result)
-        assert result.exit_code == 0
-
-    def test_info(self):
-        runner = CliRunner()
-        result = runner.invoke(info.cli, ['popperized/npm'])
-        assert result.exit_code == 0
-
-    def test_scaffold(self):
-        runner = CliRunner()
-        result = runner.invoke(scaffold.cli)
-        assert result.exit_code == 0
-
-    def test_search(self):
-        runner = CliRunner()
-        result = runner.invoke(search.cli, ['popperized/npm'])
-        assert result.exit_code == 0
+class TestCommandVersion(unittest.TestCase):
 
     def test_version(self):
         runner = CliRunner()
         result = runner.invoke(version.cli)
         assert result.exit_code == 0
+
+class TestSkip(unittest.TestCase):
+
+    def setUp(self):
+        os.makedirs('/tmp/test_folder')
+        os.chdir('/tmp/test_folder')
+        log.setLevel('CRITICAL')
+
+    def tearDown(self):
+        os.chdir('/tmp')
+        shutil.rmtree('/tmp/test_folder')
+        log.setLevel('NOTSET')
+
+    def create_workflow_file(self, content):
+        f = open('/tmp/test_folder/a.workflow', 'w')
+        f.write(content)
+        f.close()
+
+    def test_skip(self):
+
+        self.create_workflow_file("""
+        workflow "wf" {
+          resolves = "d"
+        }
+
+        action "a1" {
+          uses = "sh"
+          args = "ls"
+        }
+
+        action "a2" {
+          uses = "sh"
+          args = "ls"
+        }
+
+        action "b" {
+          needs = ["a1", "a2"]
+          uses = "sh"
+          args = "ls"
+        }
+
+        action "c" {
+          needs = ["a1", "a2"]
+          uses = "sh"
+          args = "ls"
+        }
+
+        action "d" {
+          needs = ["b", "c"]
+          uses = "sh"
+          args = "ls"
+        }
+        """)
+
+        runner = CliRunner()
+        result = runner.invoke(run.cli,['a1','--wfile','/tmp/test_folder/a.workflow','--skip','a2'])
+        assert result.exit_code == 1
+
+        result = runner.invoke(run.cli,['--dry-run','--wfile','/tmp/test_folder/a.workflow','a1'])
+        assert result.exit_code == 0
+
+        result = runner.invoke(run.cli,['--dry-run','--wfile','/tmp/test_folder/a.workflow','a1'])
+        assert result.exit_code == 0
+
+        result = runner.invoke(run.cli,['--dry-run','--wfile','/tmp/test_folder/a.workflow','a2'])
+        assert result.exit_code == 0
+
+        result = runner.invoke(run.cli,['--dry-run','--wfile','/tmp/test_folder/a.workflow','b'])
+        assert result.exit_code == 0
+
+        result = runner.invoke(run.cli,['--dry-run','--wfile','/tmp/test_folder/a.workflow','c'])
+        assert result.exit_code == 0
+
+        result = runner.invoke(run.cli,['--dry-run','--wfile','/tmp/test_folder/a.workflow','d'])
+        assert result.exit_code == 0
+
+        result = runner.invoke(run.cli,['--dry-run','--wfile','/tmp/test_folder/a.workflow','--skip','e1'])
+        assert result.exit_code == 1
+
+        result = runner.invoke(run.cli,['--dry-run','--wfile','/tmp/test_folder/a.workflow','--skip','a1','--skip','a2'])
+        assert result.exit_code == 1
