7bb2e4f33c3655046885d0a0447954d7316839a0
Vipul Chhabra <vipulchhabra555@gmail.com>
2020-02-16 14:30:39 +0530

    PEP8 Fixes

--- cli/test/test_common.py
+++ cli/test/test_common.py
@@ -10,43 +10,45 @@ os.environ['CI'] = 'false'
 
 pushstack = list()
 
+
 def pushdir(dirname):
-  global pushstack
-  pushstack.append(os.getcwd())
-  os.chdir(dirname)
+    global pushstack
+    pushstack.append(os.getcwd())
+    os.chdir(dirname)
+
 
 def popdir():
-  global pushstack
-  os.chdir(pushstack.pop())
+    global pushstack
+    os.chdir(pushstack.pop())
+
 
 def delete_dir(path):
 
-	try:
-		os.rmdir(path)
-	except OSError as e:
-		os.system("docker run --rm -v /tmp:/tmp alpine:3.8 rm -rf "+path)
+    try:
+        os.rmdir(path)
+    except OSError as e:
+        os.system("docker run --rm -v /tmp:/tmp alpine:3.8 rm -rf "+path)
+
 
 def init_test_repo():
 
-	if(os.environ.get('POPPER_TEST_MODE') == 'with-git'):
-		path = os.environ.get('test_repo_path')
-		delete_dir(path)
-		os.mkdir(path)
-		pushdir(path)
-		repo = Repo.init(path)
-		file = open(path+'/README.md','w+')
-		file.write("foo")
-		repo.git.add(A=True)
-		repo.git('-m','first')
-		popdir()
+    if(os.environ.get('POPPER_TEST_MODE') == 'with-git'):
+        path = os.environ.get('test_repo_path')
+        delete_dir(path)
+        os.mkdir(path)
+        pushdir(path)
+        repo = Repo.init(path)
+        file = open(path+'/README.md', 'w+')
+        file.write("foo")
+        repo.git.add(A=True)
+        repo.git('-m', 'first')
+        popdir()
 
-	else:
-		path = os.environ.get('test_repo_path')
-		delete_dir(path)
-		os.mkdir(path)
-		pushdir(path)
-		file = open(path+'/README.md','w+')
-		file.write("foo")
-		popdir()
-
-	
+    else:
+        path = os.environ.get('test_repo_path')
+        delete_dir(path)
+        os.mkdir(path)
+        pushdir(path)
+        file = open(path+'/README.md', 'w+')
+        file.write("foo")
+        popdir()
