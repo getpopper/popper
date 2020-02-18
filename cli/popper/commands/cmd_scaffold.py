4184c768b9d6ae354ec1a6cb0f788d6d46f3e8d8
Edidiong Etuk <36479528+edeediong@users.noreply.github.com>
2020-02-18 09:30:24 +0800

    docs: removes docstrings from click text messages (#778)
    adds reference to `--help` flag in Getting Started guide. It also removes function
    arguments from click-annotated functions as these are shown by click.
    
    fixes #774  
    fixes #775

--- cli/popper/commands/cmd_scaffold.py
+++ cli/popper/commands/cmd_scaffold.py
@@ -9,16 +9,16 @@ from popper.cli import pass_context, log
 @pass_context
 def cli(ctx):
     """Scaffolds a workflow.
-
-    Args:
-      ctx(Popper.cli.context): For process inter-command communication
-            context is used.For reference visit
-            https://click.palletsprojects.com/en/7.x/commands/#nested-handling-and-contexts .
-
-    Returns:
-        None
-
     """
+
+    # Args:
+    #   ctx(Popper.cli.context): For process inter-command communication
+    #         context is used.For reference visit
+    #         https://click.palletsprojects.com/en/7.x/commands/#nested-handling-and-contexts .
+
+    # Returns:
+    #     None
+
     curr_dir = os.getcwd()
     actions_dir = os.path.join(curr_dir, 'actions')
 
--- cli/popper/commands/cmd_version.py
+++ cli/popper/commands/cmd_version.py
@@ -8,14 +8,14 @@ from popper.cli import pass_context, log
 @pass_context
 def cli(ctx):
     """Displays version of Popper and exit.
-
-    Args:
-      ctx(Popper.cli.context): For process inter-command communication
-            context is used.For reference visit
-            https://click.palletsprojects.com/en/7.x/commands/#nested-handling-and-contexts .
-
-    Returns:
-      None
-
     """
+
+    # Args:
+    #   ctx(Popper.cli.context): For process inter-command communication
+    #         context is used.For reference visit
+    #         https://click.palletsprojects.com/en/7.x/commands/#nested-handling-and-contexts .
+
+    # Returns:
+    #   None
+
     log.info('Popper version {}'.format(popper_version))
