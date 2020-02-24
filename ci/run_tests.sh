d1e1cc837d96af0ea2387bf8cb323ed04778275c
Ivo Jimenez <ivo.jimenez@gmail.com>
2020-02-24 00:50:38 -0800

    disables wip tests

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
+set -e
 
-ci/test/reuse
+if [ -z "$ENGINE" ]; then
+  echo "Expecting ENGINE variable"
+  exit 1
+fi
+
+echo "###################################"
+echo "actions-demo"
 ci/test/actions-demo
+echo "###################################"
+echo "reuse"
+ci/test/reuse
+echo "###################################"
 ci/test/validate
+echo "###################################"
 ci/test/scaffold
+echo "###################################"
 ci/test/dry-run
-ci/test/parallel
+echo "###################################"
+# ci/test/parallel
+echo "###################################"
 ci/test/dot
-ci/test/interrupt
+echo "###################################"
+# ci/test/interrupt
+echo "###################################"
 ci/test/quiet
+echo "###################################"
 ci/test/sh
+echo "###################################"
 ci/test/skip
+echo "###################################"
 ci/test/substitutions
+echo "###################################"
 ci/test/offline
+echo "###################################"
 ci/test/inject
+echo "###################################"
 ci/test/engine-conf
