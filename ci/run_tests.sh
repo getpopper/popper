a54a77d224116025ed326cd343243fd8881bc50e
Ivo Jimenez <ivo.jimenez@gmail.com>
2020-02-24 01:10:42 -0700

    Removes popper pipeline for ci

---
+++ ci/run_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ci/test/reuse
+ci/test/actions-demo
+ci/test/validate
+ci/test/scaffold
+ci/test/dry-run
+ci/test/parallel
+ci/test/dot
+ci/test/interrupt
+ci/test/quiet
+ci/test/sh
+ci/test/skip
+ci/test/substitutions
+ci/test/offline
+ci/test/inject
+ci/test/engine-conf
