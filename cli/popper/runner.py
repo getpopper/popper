d80537dbc764333d614d24d95f73c4affd50d363
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-03-25 15:43:32 +0530

    Refactoring

--- cli/popper/runner.py
+++ cli/popper/runner.py
@@ -32,18 +32,21 @@ class WorkflowRunner(object):
         # cretate a repo object for the project
         self.repo = scm.new_repo()
         self.config.workspace_sha = scm.get_sha(self.repo)
+        
         self.config.engine_name = engine
         self.config.resman_name = resource_manager
+        self.popper_cfg = DotMap()
 
         if config_file:
-            popper_cfg = PopperConfig(config_file)
-            self.config.engine_name = pu.select_not_none(
-                [self.config.engine_name, popper_cfg.engine.name, 'docker'])
-            self.config.resman_name = pu.select_not_none(
-                [self.config.resman_name, popper_cfg.resource_manager.name, 'host'])
+            self.popper_cfg = PopperConfig(config_file)
+        
+        self.config.engine_name = pu.select_not_none(
+            [self.config.engine_name, self.popper_cfg.engine.name, 'docker'])
+        self.config.resman_name = pu.select_not_none(
+            [self.config.resman_name, self.popper_cfg.resource_manager.name, 'host'])
 
-            self.config.resman_options = popper_cfg.resource_manager.options
-            self.config.engine_options = popper_cfg.engine.options
+        self.config.resman_options = self.popper_cfg.resource_manager.options
+        self.config.engine_options = self.popper_cfg.engine.options
 
         # dynamically load resource manager
         resman_mod_name = f'popper.runner_{self.config.resman_name}'
