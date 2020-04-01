ef318bd85e399b919c3752e3a20fcba719e70e3a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-04-01 20:13:49 +0530

    Add exec_cmd test

--- cli/test/test_utils.py
+++ cli/test/test_utils.py
@@ -104,3 +104,19 @@ engine:
 
         b = [DotMap(), "Hello", []]
         self.assertEqual(pu.select_not_none(b), "Hello")
+
+    def test_exec_cmd(self):
+        cmd = ["echo", "command_1"]
+        ecode, output = pu.exec_cmd(cmd, logging=False)
+        self.assertEqual(ecode, 0)
+        self.assertEqual(output, "command_1\n")
+
+        ecode, output = pu.exec_cmd(cmd, logging=True)
+        self.assertEqual(ecode, 0)
+        self.assertEqual(output, "")
+
+        pu.write_file("/tmp/test.py", "import os\nprint(os.environ['TEST'])")
+        cmd = ["python", "test.py"]
+        ecode, output = pu.exec_cmd(cmd, env={ 'TEST': 'test' }, cwd="/tmp", logging=False)
+        self.assertEqual(ecode, 0)
+        self.assertEqual(output, "test\n")
