22c7408788f9c11c124e652fe6585e22fd7d5028
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-04-11 19:10:34 +0530

    Seperate test dependencies

--- cli/setup.py
+++ cli/setup.py
@@ -14,9 +14,6 @@ setup(
     packages=['popper', 'popper.commands'],
     include_package_data=True,
     install_requires=[
-        'sh',
-        'psutil',
-        'testfixtures',
         'python-vagrant',
         'GitPython',
         'spython',
@@ -25,6 +22,9 @@ setup(
         'pyyaml',
         'docker'
     ],
+    extras_require={
+        'dev': ['psutil', 'testfixtures']
+    },
     entry_points='''
         [console_scripts]
         popper=popper.cli:cli
