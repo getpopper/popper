d2b434b4b3365045298ba13eaf8aeedaf788df14
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-04-15 00:47:08 +0530

    Use context managers in test_runner_host.py

--- cli/test/test_runner_host.py
+++ cli/test/test_runner_host.py
@@ -86,9 +86,9 @@ class TestHostHostRunner(unittest.TestCase):
 
     def test_stop_running_tasks(self):
         pid = Popen(["sleep", "2000"]).pid
-        host_runner = HostRunner()
-        host_runner._spawned_pids.add(pid)
-        host_runner.stop_running_tasks()
+        with HostRunner() as hr:
+            hr._spawned_pids.add(pid)
+            hr.stop_running_tasks()
         time.sleep(2)
         proc = psutil.Process(pid)
         self.assertEqual(proc.status(), psutil.STATUS_ZOMBIE)
@@ -107,24 +107,24 @@ class TestHostDockerRunner(unittest.TestCase):
             'name': 'kontainer_one'
         }
         cid = pu.sanitized_name(step['name'], config.wid)
-        docker_runner = DockerRunner(init_docker_client=True, config=config)
-        c = docker_runner._create_container(cid, step)
-        self.assertEqual(c.status, 'created')
-        c.remove()
+        with DockerRunner(init_docker_client=True, config=config) as dr:
+            c = dr._create_container(cid, step)
+            self.assertEqual(c.status, 'created')
+            c.remove()
 
     @unittest.skipIf(os.environ['ENGINE'] != 'docker', 'ENGINE != docker')
     def test_stop_running_tasks(self):
-        dclient = docker.from_env()
-        docker_runner = DockerRunner()
-        c1 = dclient.containers.run(
-            'debian:buster-slim', 'sleep 20000', detach=True)
-        c2 = dclient.containers.run('alpine:3.9', 'sleep 10000', detach=True)
-        docker_runner._spawned_containers.add(c1)
-        docker_runner._spawned_containers.add(c2)
-        docker_runner.stop_running_tasks()
-        self.assertEqual(c1.status, 'created')
-        self.assertEqual(c2.status, 'created')
-        dclient.close()
+        with DockerRunner() as dr:
+            dclient = docker.from_env()
+            c1 = dclient.containers.run(
+                'debian:buster-slim', 'sleep 20000', detach=True)
+            c2 = dclient.containers.run('alpine:3.9', 'sleep 10000', detach=True)
+            dr._spawned_containers.add(c1)
+            dr._spawned_containers.add(c2)
+            dr.stop_running_tasks()
+            self.assertEqual(c1.status, 'created')
+            self.assertEqual(c2.status, 'created')
+            dclient.close()
 
     @unittest.skipIf(os.environ['ENGINE'] != 'docker', 'ENGINE != docker')
     def test_get_container_kwargs(self):
@@ -155,25 +155,25 @@ class TestHostDockerRunner(unittest.TestCase):
             config_file=config_dict,
             workspace_dir='/path/to/workdir')
 
-        docker_runner = DockerRunner(init_docker_client=False, config=config)
-        args = docker_runner._get_container_kwargs(
-            step, 'alpine:3.9', 'container_a')
+        with DockerRunner(init_docker_client=False, config=config) as dr:
+            args = dr._get_container_kwargs(
+                step, 'alpine:3.9', 'container_a')
 
-        self.assertEqual(args, {
-            'image': 'alpine:3.9',
-            'command': ['ls'],
-            'name': 'container_a',
-            'volumes': [
-                '/path/to/workdir:/workspace',
-                '/var/run/docker.sock:/var/run/docker.sock',
-                '/path/in/host:/path/in/container'],
-            'working_dir': '/workspace',
-            'environment': {'FOO': 'bar'},
-            'entrypoint': None,
-            'detach': True,
-            'privileged': True,
-            'hostname': 'popper.local',
-            'domainname': 'www.example.org'})
+            self.assertEqual(args, {
+                'image': 'alpine:3.9',
+                'command': ['ls'],
+                'name': 'container_a',
+                'volumes': [
+                    '/path/to/workdir:/workspace',
+                    '/var/run/docker.sock:/var/run/docker.sock',
+                    '/path/in/host:/path/in/container'],
+                'working_dir': '/workspace',
+                'environment': {'FOO': 'bar'},
+                'entrypoint': None,
+                'detach': True,
+                'privileged': True,
+                'hostname': 'popper.local',
+                'domainname': 'www.example.org'})
 
     @unittest.skipIf(os.environ['ENGINE'] != 'docker', 'ENGINE != docker')
     def test_get_build_info(self):
@@ -183,27 +183,28 @@ class TestHostDockerRunner(unittest.TestCase):
             'name': 'one',
             'repo_dir': '/path/to/repo/dir',
             'step_dir': 'sh'}
-        docker_runner = DockerRunner(init_docker_client=False)
-        build, img, tag, build_sources = docker_runner._get_build_info(step)
-        self.assertEqual(build, True)
-        self.assertEqual(img, 'popperized/bin')
-        self.assertEqual(tag, 'master')
-        self.assertEqual(
-            build_sources,
-            '/path/to/repo/dir/sh')
+        with DockerRunner(init_docker_client=False) as dr:
+            build, img, tag, build_sources = dr._get_build_info(step)
+            self.assertEqual(build, True)
+            self.assertEqual(img, 'popperized/bin')
+            self.assertEqual(tag, 'master')
+            self.assertEqual(
+                build_sources,
+                '/path/to/repo/dir/sh')
 
-        step = {
-            'uses': 'docker://alpine:3.9',
-            'runs': ['sh', '-c', 'echo $FOO > hello.txt ; pwd'],
-            'env': {'FOO': 'bar'},
-            'name': '1'
-        }
-        docker_runner = DockerRunner(init_docker_client=False)
-        build, img, tag, build_sources = docker_runner._get_build_info(step)
-        self.assertEqual(build, False)
-        self.assertEqual(img, 'alpine')
-        self.assertEqual(tag, '3.9')
-        self.assertEqual(build_sources, None)
+            step = {
+                'uses': 'docker://alpine:3.9',
+                'runs': ['sh', '-c', 'echo $FOO > hello.txt ; pwd'],
+                'env': {'FOO': 'bar'},
+                'name': '1'
+            }
+
+        with DockerRunner(init_docker_client=False) as dr:
+            build, img, tag, build_sources = dr._get_build_info(step)
+            self.assertEqual(build, False)
+            self.assertEqual(img, 'alpine')
+            self.assertEqual(tag, '3.9')
+            self.assertEqual(build_sources, None)
 
     @unittest.skipIf(os.environ['ENGINE'] != 'docker', 'ENGINE != docker')
     def test_docker_basic_run(self):
