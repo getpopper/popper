dc84e4dd160f91b9083b75f8615f3d5f4567cb38
Edidiong Etuk <36479528+edeediong@users.noreply.github.com>
2020-04-19 12:32:31 +0100

    Update install-test-deps.sh

--- ci/scripts/install-test-deps.sh
+++ ci/scripts/install-test-deps.sh
@@ -21,5 +21,7 @@ fi
 if [[ $RESMAN == "kubernetes" ]]; then
   git clone --depth 1 -b "v0.7.0-2" --single-branch https://github.com/k8s-school/kind-travis-ci.git
   ./kind-travis-ci/kind/k8s-create.sh
+  rm -r $HOME/.kube
+  mkdir $HOME/.kube
   cp $(kind get kubeconfig-path --name="kind") $HOME/.kube/config
 fi
