c0a62ea2b678adad023eaced83cbf00c5dd8c517
Vipul Chhabra <vipulchhabra555@gmail.com>
2020-04-23 01:32:28 +0530

    adds 'popper dot' test (#810)
    * Adds 'dot' test
    
    * Variable Rename
    
    * Removed scaffold and dot CI test

---
+++ cli/test/test_cmddot.py
@@ -0,0 +1,43 @@
+from click.testing import CliRunner
+
+import popper.commands.cmd_dot as dot
+import popper.commands.cmd_run as run
+from test_common import PopperTest
+from popper.parser import Workflow, YMLWorkflow, HCLWorkflow
+
+import unittest
+import os
+import tempfile
+import git
+
+
+class TestDot(PopperTest):
+
+    def test_dot(self):
+
+        wf_dir = tempfile.mkdtemp()
+        runner = CliRunner()
+        git.Git(wf_dir).clone(
+            "https://github.com/popperized/github-actions-demo.git")
+
+        workflow_path = f'{wf_dir}/github-actions-demo/.github/main.workflow'
+
+        with self.assertLogs('popper') as test_logger:
+
+            result = runner.invoke(dot.cli, ['-f', workflow_path])
+            self.assertEqual(result.exit_code, 0)
+            log_output = test_logger.output[0]
+            self.assertTrue('"branch-filter" -> "deploy";' in log_output)
+            self.assertTrue('"install" -> "lint";' in log_output)
+            self.assertTrue('"install" -> "test";' in log_output)
+            self.assertTrue('"lint" -> "branch-filter";' in log_output)
+            self.assertTrue('"test-and-deploy" -> "install";' in log_output)
+            self.assertTrue('"test" -> "branch-filter"' in log_output)
+            self.assertFalse('fillcolor=transparent,color=' in log_output)
+
+        with self.assertLogs('popper') as test_logger:
+
+            result = runner.invoke(dot.cli, ['--colors', '-f', workflow_path])
+            self.assertEqual(result.exit_code, 0)
+            self.assertTrue(
+                'fillcolor=transparent,color=' in test_logger.output[0])
