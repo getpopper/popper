ae160100874cc3daace6e67cf6452e8241cb2687
Ivo Jimenez <ivo.jimenez@gmail.com>
2020-04-26 20:30:27 +0000

    Creates initial two tests for kubernetes

--- cli/test/test_runner_kubernetes.py
+++ cli/test/test_runner_kubernetes.py
@@ -1,11 +1,10 @@
 import os
 import unittest
 
+from popper.cli import log as log
 from popper.config import PopperConfig
 from popper.parser import YMLWorkflow
 from popper.runner import WorkflowRunner
-
-from popper.cli import log as log
 
 from .test_common import PopperTest
 
@@ -15,11 +14,21 @@ class TestKubernetesDockerRunner(PopperTest):
         log.setLevel('CRITICAL')
 
     @unittest.skipIf(
-        os.environ['ENGINE'] != 'docker' or os.environ['RESMAN'] != 'kubernetes',
-        'ENGINE != docker or RESMAN != kubernetes')
-    def test_docker_basic_run(self):
+        not os.environ.get('WITH_K8S', None) or
+        os.environ.get('ENGINE', 'docker') != 'docker',
+        'WITH_K8S not defined or ENGINE != docker')
+    def test_docker_build_image(self):
         repo = self.mk_repo()
-        conf = PopperConfig(workspace_dir=repo.working_dir, resman_name='kubernetes')
+
+        # NOTE: if you are testing locally and you are not authorized to push
+        #       to the popperized repository, then you need to change this
+        #       with the your docker.io username so that the test can pass.
+        config_opts = {
+            'registry_user': 'popperized',
+        }
+        conf = PopperConfig(workspace_dir=repo.working_dir,
+                            resman_name='kubernetes',
+                            config_file=config_opts)
 
         with WorkflowRunner(conf) as r:
             wf = YMLWorkflow("""
@@ -30,7 +39,22 @@ class TestKubernetesDockerRunner(PopperTest):
               args: README.md
             """)
             wf.parse()
+
+            # this should cause a container to be built locally,
+            # pushed to the popperized/bin repo, and then the image
+            # be pulled by kubernetes when setting up the pod
             r.run(wf)
+
+    @unittest.skipIf(
+        not os.environ.get('WITH_K8S', None) or
+        os.environ.get('ENGINE', 'docker') != 'docker',
+        'WITH_K8S not defined or ENGINE != docker')
+    def test_docker_write_to_volume(self):
+        repo = self.mk_repo()
+        conf = PopperConfig(workspace_dir=repo.working_dir,
+                            resman_name='kubernetes')
+
+        with WorkflowRunner(conf) as r:
 
             wf = YMLWorkflow("""
             version: '1'
@@ -43,16 +67,8 @@ class TestKubernetesDockerRunner(PopperTest):
             """)
             wf.parse()
             r.run(wf)
-            with open(os.path.join(repo.working_dir, 'hello.txt'), 'r') as f:
-                self.assertEqual(f.read(), 'bar\n')
 
-            wf = YMLWorkflow("""
-            version: '1'
-            steps:
-            - uses: 'docker://alpine:3.9'
-              runs: 'nocommandisnamedlikethis'
-            """)
-            wf.parse()
-            self.assertRaises(Exception, r.run, wf)
+            # TODO: use kubernetes client to assert that the file was
+            #       created in the volume associated to the pod
 
         repo.close()
