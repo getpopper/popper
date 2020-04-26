f0eb3798669ca979d9f80c6a17d8ae518e184ea3
Ivo Jimenez <ivo.jimenez@gmail.com>
2020-04-26 19:27:41 +0000

    restore install-deps from upstream

--- ci/scripts/install-test-deps.sh
+++ ci/scripts/install-test-deps.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 set -ex
 
-if [[ $ENGINE == "singularity" ]]; then
-  sudo apt-get update
-  sudo apt-get install -y build-essential libssl-dev uuid-dev libgpgme11-dev libseccomp-dev pkg-config squashfs-tools
-  mkdir -p ${GOPATH}/src/github.com/sylabs
-  cd ${GOPATH}/src/github.com/sylabs
-  git clone https://github.com/sylabs/singularity.git
-  cd singularity
-  git checkout v3.2.0
-  cd ${GOPATH}/src/github.com/sylabs/singularity
-  ./mconfig
-  cd ./builddir
-  make
-  sudo make install
-  singularity version
-  cd $TRAVIS_BUILD_DIR
-fi
-
-if [[ $RESMAN == "kubernetes" ]]; then
-  git clone --depth 1 -b "v0.7.0-2" --single-branch https://github.com/k8s-school/kind-travis-ci.git
-  ./kind-travis-ci/kind/k8s-create.sh
-  mkdir $HOME/.kube
-  cp $(kind get kubeconfig-path --name="kind") $HOME/.kube/config
+if [ "$ENGINE" == "singularity" ]; then
+    sudo add-apt-repository 'deb http://ftp.de.debian.org/debian bullseye main'
+    sudo apt-get update
+    wget http://ftp.us.debian.org/debian/pool/main/s/singularity-container/singularity-container_3.5.2+ds1-1_amd64.deb
+    sudo apt-get -f --allow-unauthenticated -y install ./singularity-container_3.5.2+ds1-1_amd64.deb
+    singularity version
 fi
