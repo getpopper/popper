89f20ff8fa440585de5f611e94a1141ba5ad4297
Ivo Jimenez <ivo.jimenez@gmail.com>
2020-05-14 12:15:41 -0700

    Update commands to reflect popper.config and popper.parser changes

--- cli/test/test_cmd_dot.py
+++ cli/test/test_cmd_dot.py
@@ -1,37 +1,46 @@
 import tempfile
-import git
 
 from click.testing import CliRunner
 
 import popper.commands.cmd_dot as dot
 
+from popper.cli import log
+
 from .test_common import PopperTest
 
 
 class TestDot(PopperTest):
+    def setUp(self):
+        log.setLevel("CRITICAL")
+
     def test_dot(self):
+        with tempfile.NamedTemporaryFile(mode="w+t", suffix=".yml") as f:
+            f.write(
+                """
+steps:
+- id: one
+  uses: 'foo/bar'
+- id: two
+  uses: 'foo/bar'
+- id: three
+  uses: 'foo/bar'
+"""
+            )
+            f.flush()
 
-        wf_dir = tempfile.mkdtemp()
-        runner = CliRunner()
-        git.Git(wf_dir).clone("https://github.com/popperized/github-actions-demo.git")
+            runner = CliRunner()
 
-        workflow_path = f"{wf_dir}/github-actions-demo/.github/main.workflow"
+            with self.assertLogs("popper") as test_logger:
+                result = runner.invoke(dot.cli, ["-f", f.name])
+                log.debug(f"file: {f.name}")
+                self.assertEqual(result.exit_code, 0)
+                log_output = test_logger.output[0]
+                self.assertTrue('"one" -> "two";' in log_output)
+                self.assertTrue('"two" -> "three";' in log_output)
+                self.assertFalse("fillcolor=transparent,color=" in log_output)
 
-        with self.assertLogs("popper") as test_logger:
+            with self.assertLogs("popper") as test_logger:
 
-            result = runner.invoke(dot.cli, ["-f", workflow_path])
-            self.assertEqual(result.exit_code, 0)
-            log_output = test_logger.output[0]
-            self.assertTrue('"branch-filter" -> "deploy";' in log_output)
-            self.assertTrue('"install" -> "lint";' in log_output)
-            self.assertTrue('"install" -> "test";' in log_output)
-            self.assertTrue('"lint" -> "branch-filter";' in log_output)
-            self.assertTrue('"test-and-deploy" -> "install";' in log_output)
-            self.assertTrue('"test" -> "branch-filter"' in log_output)
-            self.assertFalse("fillcolor=transparent,color=" in log_output)
-
-        with self.assertLogs("popper") as test_logger:
-
-            result = runner.invoke(dot.cli, ["--colors", "-f", workflow_path])
-            self.assertEqual(result.exit_code, 0)
-            self.assertTrue("fillcolor=transparent,color=" in test_logger.output[0])
+                result = runner.invoke(dot.cli, ["--colors", "-f", f.name])
+                self.assertEqual(result.exit_code, 0)
+                self.assertTrue("fillcolor=transparent,color=" in test_logger.output[0])
