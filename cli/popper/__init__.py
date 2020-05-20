9bbbe3340daea7161230a219fe2381603ba2a622
Fernanda Domínguez <fernanda.dominguezac@gmail.com>
2020-05-19 23:47:42 -0700

    use dunamai to display versioning information (#831)
    makes use of the dunamai package to display versioning information (`popper version`). In addition, this commit adds a `--version` flag to the `popper` command.
    
    fixes #783

--- cli/popper/__init__.py
+++ cli/popper/__init__.py
@@ -1,1 +1,21 @@
-__version__ = "2.6.0-dev"
+import importlib
+import pathlib
+import os
+
+
+# check if dunamai is available
+dunamai_spec = importlib.util.find_spec("dunamai")
+dunamai_found = dunamai_spec is not None
+if dunamai_found:
+    # if dunamai is found, then we use it to display the version
+    import dunamai
+
+    __version__ = dunamai.Version.from_any_vcs().serialize()
+    _ver = f'__popper_version__ = "{__version__}"'
+
+    _init_script_dir = pathlib.Path(__file__).parent.absolute()
+    _version_path_ = os.path.join(_init_script_dir, "_version.py")
+    with open(_version_path_, "w") as v:
+        v.write(_ver)
+else:
+    from popper._version import __popper_version__ as __version__
--- cli/test/test_cmd_version.py
+++ cli/test/test_cmd_version.py
@@ -1,8 +1,8 @@
 from click.testing import CliRunner
 
 from popper import __version__
+from popper._version import __popper_version__
 from popper.commands import cmd_version
-
 from .test_common import PopperTest
 
 
@@ -13,5 +13,6 @@ class TestCommandVersion(PopperTest):
             runner = CliRunner()
             result = runner.invoke(cmd_version.cli)
             self.assertEqual(result.exit_code, 0)
-
+            self.assertIsNot(__version__, "0.0.0")
+            self.assertEqual(__version__, __popper_version__)
         self.assertTrue(__version__ in test.output[0])
