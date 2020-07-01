b49c54005e8fcb86290ecea98b46904c2c0f3ff4
Edidiong Etuk <edidiongmichael08@gmail.com>
2020-07-01 10:17:33 +0100

    fix podman builds in travis

--- src/scripts/install_podman.sh
+++ src/scripts/install_podman.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-if [ "ENGINE" == "podman" ]; then
+if [ "$ENGINE" == "podman" ]; then
     . /etc/os-release
     echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/testing/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list
     curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/testing/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
