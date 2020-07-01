6b0d9a35c3495970d2da6e96469c00ab34911146
Edidiong Etuk <edidiongmichael08@gmail.com>
2020-07-01 09:06:48 +0100

    added podman step to travis

---
+++ src/scripts/install_podman.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+if [ "ENGINE" == "podman" ]; then
+    . /etc/os-release
+    echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/testing/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list
+    curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/testing/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
+    sudo apt-get update -qq
+    sudo apt-get -qq -y install podman
+    podman version
+fi
