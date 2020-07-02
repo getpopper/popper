681804ade751e2fe13d36a9734d1eb390c99c1b3
Edidiong Etuk <edidiongmichael08@gmail.com>
2020-07-02 16:56:01 +0100

    formatting codebase

--- src/test/test_runner_host.py
+++ src/test/test_runner_host.py
@@ -346,7 +346,9 @@ class TestHostPodmanRunner(PopperTest):
         with PodmanRunner() as pr:
             cmd = ["podman", "run", "-d"]
             _, _, c1 = HostRunner._exec_cmd(
-                cmd + ["debian:buster-slim", "sleep", "20000", "-q", "--network", "host"], logging=False
+                cmd
+                + ["debian:buster-slim", "sleep", "20000", "-q", "--network", "host"],
+                logging=False,
             )
             _, _, c2 = HostRunner._exec_cmd(
                 cmd + ["alpine:3.9", "sleep", "10000", "-q"], logging=False
