fab25cc763d15e2886868c67492f0b71cccf5b5a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-07-24 20:28:40 +0530

    fix k8s install script

--- src/scripts/install_k8s.sh
+++ src/scripts/install_k8s.sh
@@ -5,8 +5,7 @@ if [[ -n "$WITH_K8S" ]]; then
   # download kind
   curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64"
   chmod +x ./kind
-  mv ./kind /some-dir-in-your-PATH/kind
 
   # create cluster
-  kind create cluster
+  ./kind create cluster
 fi
