ca30c74ee6d61687738dad34bb402cc2dae7c9cc
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-07-30 19:46:27 +0530

    update k8s install script to make pv

--- src/scripts/install_k8s.sh
+++ src/scripts/install_k8s.sh
@@ -3,9 +3,30 @@ set -ex
 
 if [[ -n "$WITH_K8S" ]]; then
   # download kind
-  curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64"
-  chmod +x ./kind
+curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64"
+chmod +x ./kind
 
-  # create cluster
-  ./kind create cluster
+# create cluster
+./kind create cluster
+
+cat <<EOF > pv.yaml
+kind: PersistentVolume
+apiVersion: v1
+metadata:
+  name: pv-hostpath
+  labels:
+    type: host
+spec:
+  persistentVolumeReclaimPolicy: Recycle
+  storageClassName: manual
+  capacity:
+    storage: 1Gi
+  accessModes:
+    - ReadWriteMany
+  hostPath:
+    path: "/tmp"
+EOF
+
+kubectl apply -f pv.yaml 
+
 fi
