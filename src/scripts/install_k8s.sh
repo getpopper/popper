9e729dcdfad314fe67215a9de5b8e7d16a78097d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-07-30 19:52:03 +0530

    add kubectl

--- src/scripts/install_k8s.sh
+++ src/scripts/install_k8s.sh
@@ -4,9 +4,15 @@ set -ex
 if [[ -n "$WITH_K8S" ]]; then
   # download kind
 curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
+
 chmod +x ./kind
+chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
 
 # create cluster
+kubectl version --client
 ./kind create cluster
 
 cat <<EOF > pv.yaml
