6f87a92a29bd77c6abb1de3bc63aaad5d8e9c2c9
Ivo Jimenez <ivo.jimenez@gmail.com>
2020-11-24 08:07:18 -0700

    tests: fix for podman runner
    Fix the installation of podman so that tests use the latest stable
    version rather than the development one.

--- src/scripts/install_podman.sh
+++ src/scripts/install_podman.sh
@@ -6,8 +6,8 @@ if [[ $ENGINE != "podman" ]]; then
 fi
 
 . /etc/os-release
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/testing/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/testing/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
 sudo apt-get update -qq
-sudo apt-get -qq -y install slirp4netns podman
+sudo apt-get -qq -y install podman uidmap slirp4netns
 podman version
