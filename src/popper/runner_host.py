2e7eccd452b5a776a6f90904b0f45aee89ac1fa8
Michael Royzen <michael@smartlens.ai>
2020-12-12 06:29:28 +0000

    Lint

--- src/popper/runner_host.py
+++ src/popper/runner_host.py
@@ -193,21 +193,20 @@ class DockerRunner(StepRunner):
         elif not self._config.skip_pull and not step.skip_pull:
             log.info(f"[{step.id}] docker pull {img}:{tag}")
             if not self._config.dry_run:
-                streamer = self._d.api.pull(
-                    repository=f"{img}:{tag}", decode=True,
-                )
-                
+                streamer = self._d.api.pull(repository=f"{img}:{tag}", decode=True,)
+
                 for chunk in streamer.splitlines():
                     if self._config.quiet:
                         continue
                     chunk = chunk.strip()
                     if chunk:
                         import json
+
                         chunk = json.loads(chunk)
-                        if 'id' in chunk:
-                            log.step_info(chunk['id'] + ': ' + chunk['status'])
+                        if "id" in chunk:
+                            log.step_info(chunk["id"] + ": " + chunk["status"])
                         else:
-                            log.step_info(chunk['status'])
+                            log.step_info(chunk["status"])
 
         if self._config.dry_run:
             return
