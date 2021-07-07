678c320ac55aee73b1793c2ef5f957f4de7c12c7
Shun Kashiwa <shunthedev@gmail.com>
2021-07-08 06:18:39 +0900

    supports entrypoint attributes

--- src/popper/translators/translator_drone.py
+++ src/popper/translators/translator_drone.py
@@ -7,7 +7,6 @@ class DroneTranslator(WorkflowTranslator):
         super().__init__()
 
     def translate(self, wf):
-        print(wf)
         box = Box(kind="pipeline", type="docker", name="default")
         box["steps"] = list(map(self._translate_step, wf["steps"]))
         return box.to_yaml()
@@ -17,6 +16,7 @@ class DroneTranslator(WorkflowTranslator):
         drone_step["name"] = popper_step["id"]
         drone_step["image"] = self._translate_uses(popper_step["uses"])
         drone_step["command"] = list(popper_step["args"])
+        drone_step["entrypoint"] = list(popper_step["runs"])
         return drone_step
 
     def _translate_uses(self, uses):
