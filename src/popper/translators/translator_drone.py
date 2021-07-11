32ac41f9fb1d85a1a8d5175c9d06a16818a16152
Shun Kashiwa <shunthedev@gmail.com>
2021-07-11 14:25:06 +0900

    fix error on missing Popper's optional fields

--- src/popper/translators/translator_drone.py
+++ src/popper/translators/translator_drone.py
@@ -8,15 +8,23 @@ class DroneTranslator(WorkflowTranslator):
 
     def translate(self, wf):
         box = Box(kind="pipeline", type="docker", name="default")
-        box["steps"] = list(map(self._translate_step, wf["steps"]))
+        box["steps"] = [
+            self._translate_step(step, i + 1) for i, step in enumerate(wf["steps"])
+        ]
         return box.to_yaml()
 
-    def _translate_step(self, popper_step):
+    def _translate_step(self, popper_step, i):
         drone_step = Box()
-        drone_step["name"] = popper_step["id"]
         drone_step["image"] = self._translate_uses(popper_step["uses"])
-        drone_step["command"] = list(popper_step["args"])
-        drone_step["entrypoint"] = list(popper_step["runs"])
+        if "id" in popper_step:
+            drone_step["name"] = popper_step["id"]
+        else:
+            drone_step["name"] = "Step {}".format(i)
+
+        if "args" in popper_step:
+            drone_step["command"] = list(popper_step["args"])
+        if "runs" in popper_step:
+            drone_step["entrypoint"] = list(popper_step["runs"])
         return drone_step
 
     def _translate_uses(self, uses):
