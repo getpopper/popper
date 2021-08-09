4d774031b8022ed4d8e97d12212f9d9fcf34dfd8
Shun <shunthedev@gmail.com>
2021-08-09 11:37:23 +0900

    add `task` to the list of translate target

--- src/popper/translators/translator.py
+++ src/popper/translators/translator.py
@@ -1,6 +1,5 @@
 class WorkflowTranslator(object):
     """Base class for translators."""
-
     def __init__(self):
         pass
 
@@ -10,8 +9,11 @@ class WorkflowTranslator(object):
     @staticmethod
     def get_translator(service_name):
         from popper.translators.translator_drone import DroneTranslator
+        from popper.translators.translator_task import TaskTranslator
 
         if service_name == "drone":
             return DroneTranslator()
+        elif service_name == "task":
+            return TaskTranslator()
         else:
             raise Exception(f"Unknown service {service_name}")
