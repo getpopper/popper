adcabe233d02813040553a8df96bea6a27572b91
Shun Kashiwa <shunthedev@gmail.com>
2021-08-23 17:57:00 +0900

    translator: create TaskTranslator

---
+++ src/popper/translators/translator_task.py
@@ -0,0 +1,105 @@
+from box.box import Box
+from popper.translators.translator import WorkflowTranslator
+from shlex import quote
+
+
+# helper function to quote and join strings
+def quoteJoin(lst):
+    return " ".join(quote(arg) for arg in lst)
+
+
+class TaskTranslator(WorkflowTranslator):
+    def __init__(self):
+        super().__init__()
+
+    def translate(self, wf):
+        box = Box(version="3", tasks={}, vars={"PWD": {"sh": "pwd"}})
+
+        # translate each step
+        for step in wf["steps"]:
+            box["tasks"][step["id"]] = self._translate_step(step)
+
+        # call steps in order from default task
+        box["tasks"]["default"] = {
+            "cmds": [{"task": step["id"]} for step in wf["steps"]]
+        }
+
+        return box.to_yaml()
+
+    # translate a step
+    def _translate_step(self, step):
+        t = self._detect_type(step["uses"])
+        if t == "docker":
+            return self._translate_docker_step(step)
+        elif t == "sh":
+            return self._translate_sh_step(step)
+
+    # detect step type (docker or exec)
+    def _detect_type(self, uses):
+        if "docker://" in uses:
+            return "docker"
+        if uses == "sh":
+            return "sh"
+        raise AttributeError(f"Unexpected value {uses} found in `uses` attribute")
+
+    # translate Popper steps to be executed on host
+    def _translate_sh_step(self, step):
+        task = Box()
+        if "runs" not in step:
+            # sh steps require `runs` attribute
+            raise AttributeError("Expecting 'runs' attribute in step.")
+        task["cmds"] = [
+            quoteJoin(step["runs"] + (step["args"] if "args" in step else []))
+        ]
+        return task
+
+    def _translate_docker_step(self, step):
+        task = Box()
+
+        # get the name of the docker image
+        image = self._get_docker_image(step["uses"])
+
+        # if `runs` is specified, override the entrypoint
+        # --entrypoint can only take one component. append the rest to command_args
+        # if not specified, set to None
+        entrypoint = (
+            f"--entrypoint {quote(step['runs'][0])}" if "runs" in step else None
+        )
+
+        # [COMMAND] [ARG]...
+        command_args = []
+        if "runs" in step:
+            # if `runs` is specified, take the second and later arguments
+            command_args = command_args + list(step["runs"][1:])
+        if "args" in step:
+            # if `args` is specified, append to the list
+            command_args = command_args + list(step["args"])
+
+        # use specified value or default value
+        workdir = step["dir"] if "dir" in step else "/workspace"
+
+        # Falsy values (e.g. None, []) will be omitted
+        command = [
+            "docker",
+            "run",
+            "--rm",
+            "-i",
+            "--volume {{.PWD}}:/workspace",
+            f"--workdir {workdir}",
+            entrypoint,
+            image,
+            quoteJoin(command_args),
+        ]
+
+        # omit falsy values and join without escapes
+        task["cmds"] = [" ".join([i for i in command if i])]
+        return task
+
+    # takes a `uses` value and returns the name of the docker image
+    def _get_docker_image(self, uses):
+        if "docker://" not in uses:
+            raise AttributeError(
+                "Only docker images are supported for Drone workflow translation"
+            )
+        img = uses.replace("docker://", "")
+        return img
---
+++ src/test/test_translator_task.py
@@ -0,0 +1,202 @@
+from box import Box
+
+from popper.translators.translator_task import TaskTranslator
+from .test_common import PopperTest
+
+
+class TestTaskTranslator(PopperTest):
+    def test_detect_type(self):
+        tt = TaskTranslator()
+        self.assertEqual(tt._detect_type("docker://alpine"), "docker")
+        self.assertEqual(tt._detect_type("docker://alpine:latest"), "docker")
+        self.assertEqual(tt._detect_type("sh"), "sh")
+        with self.assertRaises(AttributeError):
+            tt._detect_type("unknown")
+
+    def test_translate_sh_step(self):
+        tt = TaskTranslator()
+        self.assertEqual(
+            tt._translate_sh_step(
+                Box(
+                    {
+                        "id": "id",
+                        "uses": "sh",
+                        "runs": ["echo"],
+                        "args": ["hello world"],
+                    }
+                )
+            ),
+            Box({"cmds": ["echo 'hello world'"]}),
+        )
+        # missing `runs`
+        with self.assertRaises(AttributeError):
+            tt._translate_sh_step(Box({"id": "id", "uses": "sh", "args": ["hello"]}))
+        # without args
+        self.assertEqual(
+            tt._translate_sh_step(Box(id="id", uses="sh", runs=["echo"])),
+            Box({"cmds": ["echo"]}),
+        )
+
+    def test_get_docker_image(self):
+        tt = TaskTranslator()
+        # image name only
+        self.assertEqual(tt._get_docker_image("docker://alpine"), "alpine")
+        # image name + tag
+        self.assertEqual(
+            tt._get_docker_image("docker://alpine:latest"), "alpine:latest"
+        )
+        # path to the directly (not supported)
+        with self.assertRaises(AttributeError):
+            tt._get_docker_image("./path/to/dir")
+
+    def test_translate_docker_step(self):
+        tt = TaskTranslator()
+        # minimum
+        self.assertEqual(
+            tt._translate_docker_step(
+                Box({"id": "id", "uses": "docker://hello-world",})
+            ),
+            Box(
+                {
+                    "cmds": [
+                        "docker run --rm -i --volume {{.PWD}}:/workspace --workdir /workspace hello-world"
+                    ],
+                }
+            ),
+        )
+        # args
+        self.assertEqual(
+            tt._translate_docker_step(
+                Box({"id": "id", "uses": "docker://node:14", "args": ["index.js"]})
+            ),
+            Box(
+                {
+                    "cmds": [
+                        "docker run --rm -i --volume {{.PWD}}:/workspace --workdir /workspace node:14 index.js"
+                    ],
+                }
+            ),
+        )
+        # runs
+        self.assertEqual(
+            tt._translate_docker_step(
+                Box(
+                    {
+                        "id": "id",
+                        "uses": "docker://alpine",
+                        "runs": ["echo"],
+                        "args": ["hello world"],
+                    }
+                )
+            ),
+            Box(
+                {
+                    "cmds": [
+                        "docker run --rm -i --volume {{.PWD}}:/workspace --workdir /workspace --entrypoint echo alpine 'hello world'"
+                    ],
+                }
+            ),
+        )
+        # runs (two or more elements)
+        self.assertEqual(
+            tt._translate_docker_step(
+                Box(
+                    {
+                        "id": "id",
+                        "uses": "docker://alpine",
+                        "runs": ["/bin/sh", "-c"],
+                        "args": ["echo hello world"],
+                    }
+                )
+            ),
+            Box(
+                {
+                    "cmds": [
+                        "docker run --rm -i --volume {{.PWD}}:/workspace --workdir /workspace --entrypoint /bin/sh alpine -c 'echo hello world'"
+                    ],
+                }
+            ),
+        )
+        # workdir
+        self.assertEqual(
+            tt._translate_docker_step(
+                Box({"id": "id", "uses": "docker://hello-world", "dir": "/tmp"})
+            ),
+            Box(
+                {
+                    "cmds": [
+                        "docker run --rm -i --volume {{.PWD}}:/workspace --workdir /tmp hello-world"
+                    ],
+                }
+            ),
+        )
+
+    def test_translate(self):
+        tt = TaskTranslator()
+
+        popper_wf_sh = Box(
+            {
+                "steps": [
+                    {
+                        "id": "download",
+                        "uses": "sh",
+                        "runs": ["curl"],
+                        "args": [
+                            "-LO",
+                            "https://github.com/datasets/co2-fossil-global/raw/master/global.csv",
+                        ],
+                    }
+                ],
+            }
+        )
+        task_sh = tt.translate(popper_wf_sh)
+        self.assertEqual(
+            Box.from_yaml(task_sh),
+            Box(
+                {
+                    "version": "3",
+                    "vars": {"PWD": {"sh": "pwd"}},
+                    "tasks": {
+                        "default": {"cmds": [{"task": "download"}]},
+                        "download": {
+                            "cmds": [
+                                "curl -LO https://github.com/datasets/co2-fossil-global/raw/master/global.csv"
+                            ]
+                        },
+                    },
+                }
+            ),
+        )
+
+        popper_wf_docker = Box(
+            {
+                "steps": [
+                    {
+                        "id": "download",
+                        "uses": "docker://byrnedo/alpine-curl:0.1.8",
+                        "args": [
+                            "-LO",
+                            "https://github.com/datasets/co2-fossil-global/raw/master/global.csv",
+                        ],
+                    }
+                ],
+            }
+        )
+        task_docker = tt.translate(popper_wf_docker)
+        self.assertEqual(
+            Box.from_yaml(task_docker),
+            Box(
+                {
+                    "version": "3",
+                    "vars": {"PWD": {"sh": "pwd"}},
+                    "tasks": {
+                        "default": {"cmds": [{"task": "download"}]},
+                        "download": {
+                            "cmds": [
+                                "docker run --rm -i --volume {{.PWD}}:/workspace --workdir /workspace byrnedo/alpine-curl:0.1.8 -LO https://github.com/datasets/co2-fossil-global/raw/master/global.csv"
+                            ]
+                        },
+                    },
+                }
+            ),
+        )
