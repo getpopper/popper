0f65f5d7b997ba7a74300dfeee344ec23b1a519d
Shun Kashiwa <shunthedev@gmail.com>
2021-09-01 20:53:57 +0900

    translator: support `env` options in Popper-to-Task translator

--- src/popper/translators/translator_task.py
+++ src/popper/translators/translator_task.py
@@ -13,11 +13,36 @@ class TaskTranslator(WorkflowTranslator):
         super().__init__()
 
     def translate(self, wf):
-        box = Box(version="3", tasks={}, vars={"PWD": {"sh": "pwd"}})
+        box = Box(version="3", tasks={}, vars={})
+
+        box["vars"] = {
+            "PWD": {"sh": "pwd"},
+            # redirect stderr to /dev/null to supress warning if the directory is not a git repo
+            "GIT_COMMIT": {"sh": 'git rev-parse HEAD || echo ""'},
+            "GIT_BRANCH": {"sh": 'git branch --show-current 2>/dev/null || echo ""'},
+            "GIT_SHA_SHORT": {
+                "sh": 'git rev-parse --short HEAD 2>/dev/null || echo ""'
+            },
+            "GIT_REMOTE_ORIGIN_URL": {
+                # git config --get exists with non-zero code if the config is not set.
+                # if the remote origin url is not set, set the empty string
+                # no need for redirect since it doesn't generate an error message
+                "sh": 'git config --get remote.origin.url || echo ""'
+            },
+            "GIT_TAG": {"sh": "git tag -l --contains HEAD 2>/dev/null | head -n 1"},
+        }
+
+        box["env"] = {
+            "GIT_COMMIT": "{{.GIT_COMMIT}}",
+            "GIT_BRANCH": "{{.GIT_BRANCH}}",
+            "GIT_SHA_SHORT": "{{.GIT_SHA_SHORT}}",
+            "GIT_REMOTE_ORIGIN_URL": "{{.GIT_REMOTE_ORIGIN_URL}}",
+            "GIT_TAG": "{{.GIT_TAG}}",
+        }
 
         # translate each step
         for step in wf["steps"]:
-            box["tasks"][step["id"]] = self._translate_step(step)
+            box["tasks"][step["id"]] = self._translate_step(step, box["env"])
 
         # call steps in order from default task
         box["tasks"]["default"] = {
@@ -27,10 +52,10 @@ class TaskTranslator(WorkflowTranslator):
         return box.to_yaml()
 
     # translate a step
-    def _translate_step(self, step):
+    def _translate_step(self, step, env):
         t = self._detect_type(step["uses"])
         if t == "docker":
-            return self._translate_docker_step(step)
+            return self._translate_docker_step(step, env)
         elif t == "sh":
             return self._translate_sh_step(step)
 
@@ -51,9 +76,11 @@ class TaskTranslator(WorkflowTranslator):
         task["cmds"] = [
             quoteJoin(step["runs"] + (step["args"] if "args" in step else []))
         ]
+        if "env" in step:
+            task["env"] = step["env"]
         return task
 
-    def _translate_docker_step(self, step):
+    def _translate_docker_step(self, step, env):
         task = Box()
 
         # get the name of the docker image
@@ -75,6 +102,16 @@ class TaskTranslator(WorkflowTranslator):
             # if `args` is specified, append to the list
             command_args = command_args + list(step["args"])
 
+        # get environment variables available in this step
+        # both workflow-wide variables and step-specific variables are available thanks to the parser
+        step_env = step["env"] if "env" in step else {}
+
+        # a list of environment variables available in this context
+        # sort the keys for readability and testability
+        env_list = sorted(({**step_env, **env}).keys())
+        # --env flags that make environment variables in Docker
+        env_opt = " ".join([f"--env {varname}" for varname in env_list])
+
         # use specified value or default value
         workdir = step["dir"] if "dir" in step else "/workspace"
 
@@ -82,6 +119,7 @@ class TaskTranslator(WorkflowTranslator):
         command = [
             "docker",
             "run",
+            env_opt,
             "--rm",
             "-i",
             "--volume {{.PWD}}:/workspace",
@@ -93,6 +131,9 @@ class TaskTranslator(WorkflowTranslator):
 
         # omit falsy values and join without escapes
         task["cmds"] = [" ".join([i for i in command if i])]
+
+        if step_env:
+            task["env"] = step_env
         return task
 
     # takes a `uses` value and returns the name of the docker image
--- src/test/test_translator_task.py
+++ src/test/test_translator_task.py
@@ -5,6 +5,24 @@ from .test_common import PopperTest
 
 
 class TestTaskTranslator(PopperTest):
+    GIT_VARS = {
+        "GIT_COMMIT": {"sh": 'git rev-parse HEAD || echo ""'},
+        "GIT_BRANCH": {"sh": 'git branch --show-current 2>/dev/null || echo ""'},
+        "GIT_SHA_SHORT": {"sh": 'git rev-parse --short HEAD 2>/dev/null || echo ""'},
+        "GIT_REMOTE_ORIGIN_URL": {
+            "sh": 'git config --get remote.origin.url || echo ""'
+        },
+        "GIT_TAG": {"sh": "git tag -l --contains HEAD 2>/dev/null | head -n 1"},
+    }
+    GIT_ENV = {
+        "GIT_COMMIT": "{{.GIT_COMMIT}}",
+        "GIT_BRANCH": "{{.GIT_BRANCH}}",
+        "GIT_SHA_SHORT": "{{.GIT_SHA_SHORT}}",
+        "GIT_REMOTE_ORIGIN_URL": "{{.GIT_REMOTE_ORIGIN_URL}}",
+        "GIT_TAG": "{{.GIT_TAG}}",
+    }
+    GIT_ENV_FLAGS = "--env GIT_BRANCH --env GIT_COMMIT --env GIT_REMOTE_ORIGIN_URL --env GIT_SHA_SHORT --env GIT_TAG"
+
     def test_detect_type(self):
         tt = TaskTranslator()
         self.assertEqual(tt._detect_type("docker://alpine"), "docker")
@@ -15,6 +33,7 @@ class TestTaskTranslator(PopperTest):
 
     def test_translate_sh_step(self):
         tt = TaskTranslator()
+        # basic
         self.assertEqual(
             tt._translate_sh_step(
                 Box(
@@ -27,6 +46,21 @@ class TestTaskTranslator(PopperTest):
                 )
             ),
             Box({"cmds": ["echo 'hello world'"]}),
+        )
+        # with env
+        self.assertEqual(
+            tt._translate_sh_step(
+                Box(
+                    {
+                        "id": "id",
+                        "uses": "sh",
+                        "runs": ["echo"],
+                        "args": ["hello world"],
+                        "env": {"FOO": "foo", "BAR": "bar"},
+                    }
+                )
+            ),
+            Box({"cmds": ["echo 'hello world'"], "env": {"FOO": "foo", "BAR": "bar"},}),
         )
         # missing `runs`
         with self.assertRaises(AttributeError):
@@ -54,7 +88,7 @@ class TestTaskTranslator(PopperTest):
         # minimum
         self.assertEqual(
             tt._translate_docker_step(
-                Box({"id": "id", "uses": "docker://hello-world",})
+                Box({"id": "id", "uses": "docker://hello-world",}), {},
             ),
             Box(
                 {
@@ -64,16 +98,25 @@ class TestTaskTranslator(PopperTest):
                 }
             ),
         )
-        # args
+        # args + env
         self.assertEqual(
             tt._translate_docker_step(
-                Box({"id": "id", "uses": "docker://node:14", "args": ["index.js"]})
+                Box(
+                    {
+                        "id": "id",
+                        "uses": "docker://node:14",
+                        "args": ["index.js"],
+                        "env": {"FOO": "foo", "BAR": "bar"},
+                    }
+                ),
+                TestTaskTranslator.GIT_ENV,
             ),
             Box(
                 {
                     "cmds": [
-                        "docker run --rm -i --volume {{.PWD}}:/workspace --workdir /workspace node:14 index.js"
+                        f"docker run --env BAR --env FOO {TestTaskTranslator.GIT_ENV_FLAGS} --rm -i --volume {{{{.PWD}}}}:/workspace --workdir /workspace node:14 index.js"
                     ],
+                    "env": {"FOO": "foo", "BAR": "bar"},
                 }
             ),
         )
@@ -87,7 +130,8 @@ class TestTaskTranslator(PopperTest):
                         "runs": ["echo"],
                         "args": ["hello world"],
                     }
-                )
+                ),
+                {},
             ),
             Box(
                 {
@@ -107,7 +151,8 @@ class TestTaskTranslator(PopperTest):
                         "runs": ["/bin/sh", "-c"],
                         "args": ["echo hello world"],
                     }
-                )
+                ),
+                {},
             ),
             Box(
                 {
@@ -120,7 +165,7 @@ class TestTaskTranslator(PopperTest):
         # workdir
         self.assertEqual(
             tt._translate_docker_step(
-                Box({"id": "id", "uses": "docker://hello-world", "dir": "/tmp"})
+                Box({"id": "id", "uses": "docker://hello-world", "dir": "/tmp"}), {}
             ),
             Box(
                 {
@@ -155,7 +200,8 @@ class TestTaskTranslator(PopperTest):
             Box(
                 {
                     "version": "3",
-                    "vars": {"PWD": {"sh": "pwd"}},
+                    "vars": {"PWD": {"sh": "pwd"}, **TestTaskTranslator.GIT_VARS},
+                    "env": {**TestTaskTranslator.GIT_ENV},
                     "tasks": {
                         "default": {"cmds": [{"task": "download"}]},
                         "download": {
@@ -188,12 +234,13 @@ class TestTaskTranslator(PopperTest):
             Box(
                 {
                     "version": "3",
-                    "vars": {"PWD": {"sh": "pwd"}},
+                    "vars": {"PWD": {"sh": "pwd"}, **TestTaskTranslator.GIT_VARS},
+                    "env": {**TestTaskTranslator.GIT_ENV},
                     "tasks": {
                         "default": {"cmds": [{"task": "download"}]},
                         "download": {
                             "cmds": [
-                                "docker run --rm -i --volume {{.PWD}}:/workspace --workdir /workspace byrnedo/alpine-curl:0.1.8 -LO https://github.com/datasets/co2-fossil-global/raw/master/global.csv"
+                                f"docker run {TestTaskTranslator.GIT_ENV_FLAGS} --rm -i --volume {{{{.PWD}}}}:/workspace --workdir /workspace byrnedo/alpine-curl:0.1.8 -LO https://github.com/datasets/co2-fossil-global/raw/master/global.csv"
                             ]
                         },
                     },
